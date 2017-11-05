36136ca484648799f118c53eca4cb23e2295ca62
zaraki673 <azazel673@gmail.com>
2017-11-05 19:08:56 +0100

    Init

---
+++ plugin.py
@@ -0,0 +1,142 @@
+# Zigate Python Plugin
+#
+# Author: zaraki673
+#
+"""
+<plugin key="Zigate" name="Zigate plugin" author="gizmocuz" version="1.0.0" wikilink="http://www.domoticz.com/wiki/plugins/zigate.html" externallink="https://www.zigate.fr/">
+	<params>
+		<param field="SerialPort" label="Serial Port" width="150px" required="true" default=""/>
+		<param field="Mode6" label="Debug" width="75px">
+			<options>
+				<option label="True" value="Debug"/>
+				<option label="False" value="Normal"  default="true" />
+			</options>
+		</param>
+	</params>
+</plugin>
+"""
+import Domoticz
+
+class BasePlugin:
+	enabled = False
+	def __init__(self):
+		#self.var = 123
+		return
+
+	def onStart(self):
+		Domoticz.Log("onStart called")
+		global SerialConn
+		if Parameters["Mode6"] == "Debug":
+			Domoticz.Debugging(1)
+			with open(Parameters["HomeFolder"]+"Debug.txt", "wt") as text_file:
+				print("Started recording message for debug.", file=text_file)
+		SerialConn = Domoticz.Connection(Name="ZiGate", Transport="Serial", Protocol="None", Address=Parameters["SerialPort"], Baud=115200)
+		SerialConn.Connect()
+		ReqRcv=''
+
+		
+	def onStop(self):
+		Domoticz.Log("onStop called")
+
+	def onConnect(self, Connection, Status, Description):
+		Domoticz.Log("onConnect called")
+		global isConnected
+		if (Status == 0):
+			isConnected = True
+			Domoticz.Log("Connected successfully to: "+Parameters["SerialPort"])
+		else:
+			Domoticz.Log("Failed to connect ("+str(Status)+") to: "+Parameters["SerialPort"])
+			Domoticz.Debug("Failed to connect ("+str(Status)+") to: "+Parameters["SerialPort"]+" with error: "+Description)
+		return True
+
+	def onMessage(self, Connection, Data):
+		Domoticz.Log("onMessage called")
+		global Tmprcv
+		global ReqRcv
+		###########################################
+		Tmprcv=Data.decode(errors='ignore')
+		################## check if more than 1 sec between two message, if yes clear ReqRcv
+		lastHeartbeatDelta = (datetime.datetime.now()-self.lastHeartbeat).total_seconds()
+		if (lastHeartbeatDelta > 1):
+			ReqRcv=''
+			Domoticz.Debug("Last Message was "+str(lastHeartbeatDelta)+" seconds ago, Message clear")
+		#Wait not end of data '\r'
+		if Tmprcv.endswith('\r',0,len(Tmprcv))==False :
+			ReqRcv+=Tmprcv
+		else : # while end of data is receive
+			ReqRcv+=Tmprcv
+			########## TODO : verifier si une trame ZIA n est pas en milieu de message (2messages collés ou perturbation+ message accoller)
+			if ReqRcv.startswith("ZIA--{"):
+				Domoticz.Debug(ReqRcv)
+				ReadConf(ReqRcv)
+			if ReqRcv.startswith("ZIA33"):
+				Domoticz.Debug(ReqRcv)
+				ReadData(ReqRcv)
+			ReqRcv=''
+		self.lastHeartbeat = datetime.datetime.now()
+		return
+
+	def onCommand(self, Unit, Command, Level, Hue):
+		Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+
+	def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
+		Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+
+	def onDisconnect(self, Connection):
+		Domoticz.Log("onDisconnect called")
+
+	def onHeartbeat(self):
+		Domoticz.Log("onHeartbeat called")
+		if (SerialConn.Connected() != True):
+			SerialConn.Connect()
+		return True
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+	global _plugin
+	_plugin.onStart()
+
+def onStop():
+	global _plugin
+	_plugin.onStop()
+
+def onConnect(Connection, Status, Description):
+	global _plugin
+	_plugin.onConnect(Connection, Status, Description)
+
+def onMessage(Connection, Data):
+	global _plugin
+	_plugin.onMessage(Connection, Data)
+
+def onCommand(Unit, Command, Level, Hue):
+	global _plugin
+	_plugin.onCommand(Unit, Command, Level, Hue)
+
+def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
+	global _plugin
+	_plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
+
+def onDisconnect(Connection):
+	global _plugin
+	_plugin.onDisconnect(Connection)
+
+def onHeartbeat():
+	global _plugin
+	_plugin.onHeartbeat()
+
+	# Generic helper functions
+def DumpConfigToLog():
+	for x in Parameters:
+		if Parameters[x] != "":
+			Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+	Domoticz.Debug("Device count: " + str(len(Devices)))
+	for x in Devices:
+		Domoticz.Debug("Device:		   " + str(x) + " - " + str(Devices[x]))
+		Domoticz.Debug("Device ID:	   '" + str(Devices[x].ID) + "'")
+		Domoticz.Debug("Device Name:	 '" + Devices[x].Name + "'")
+		Domoticz.Debug("Device nValue:	" + str(Devices[x].nValue))
+		Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
+		Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+	return
