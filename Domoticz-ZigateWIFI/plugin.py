7465a03e61f4f8649bfd6996f2832815db617887
zaraki673 <azazel673@gmail.com>
2017-11-25 01:19:50 +0100

    rollback envoie commande zigatewifi
    seul la premiere commande est bien prise en compte, pas de problème en reception

--- Domoticz-ZigateWIFI/plugin.py
+++ Domoticz-ZigateWIFI/plugin.py
@@ -199,9 +199,9 @@ def ZigateEncode(Data):  # ajoute le transcodage
 
 def sendZigateCmd(cmd,length,datas) :
 	if datas =="" :
-		lineinput="01" + str(ZigateEncode(cmd)) + str(ZigateEncode(length)) + str(getChecksum(cmd,length,"0")) + "03000D" 
+		lineinput="01" + str(ZigateEncode(cmd)) + str(ZigateEncode(length)) + str(getChecksum(cmd,length,"0")) + "03" 
 	else :
-		lineinput="01" + str(ZigateEncode(cmd)) + str(ZigateEncode(length)) + str(getChecksum(cmd,length,datas)) + str(ZigateEncode(datas)) + "03000D"   
+		lineinput="01" + str(ZigateEncode(cmd)) + str(ZigateEncode(length)) + str(getChecksum(cmd,length,datas)) + str(ZigateEncode(datas)) + "03"   
 	Domoticz.Debug("sendZigateCmd - Comand send : " + str(lineinput))
 	httpConn.Send(bytes.fromhex(str(lineinput)))	
 
