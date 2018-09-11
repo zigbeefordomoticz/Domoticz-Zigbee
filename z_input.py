5796f1e66b3adf81ba897f446f2f362c248bb662
Patrck Pichon <patrick@pichon.me>
2018-09-11 22:47:33 +0200

    Fix the OnOff Cluster to 0006

--- z_input.py
+++ z_input.py
@@ -744,7 +744,7 @@ def ReadCluster(self, Devices, MsgData):
 			if sOnOff != '' :
 				sOnOff = sOnOff[0:2]  # Boolean
 				self.ListOfDevices[MsgSrcAddr]['Ep'][MsgSrcEp]['0012']=sOnOff
-				z_domoticz.MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, "0500",sOnOff)
+				z_domoticz.MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, "0006",sOnOff)
 				Domoticz.Log("ReadCluster - 0000/ff01 Saddr : " + str(MsgSrcAddr) + " On/Off : " + str(sOnOff) )
 
 
