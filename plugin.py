26079c10728b60b803aebe485f97ca0ab223771b
zaraki673 <azazel673@gmail.com>
2018-09-11 19:06:51 +0200

    maj version 2.4.0>2.4.1

--- plugin.py
+++ plugin.py
@@ -3,7 +3,7 @@
 #
 
 """
-<plugin key="Zigate" name="Zigate plugin" author="zaraki673 & pipiche38" version="2.4.0" wikilink="http://www.domoticz.com/wiki/Zigate" externallink="https://www.zigate.fr/">
+<plugin key="Zigate" name="Zigate plugin" author="zaraki673 & pipiche38" version="2.4.1" wikilink="http://www.domoticz.com/wiki/Zigate" externallink="https://www.zigate.fr/">
 	<params>
 		<param field="Mode1" label="Model" width="75px">
 			<options>
