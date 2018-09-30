b4a6b3f4d8843ce37e1f5d8caf237329c8f5bfa3
Patrck Pichon <patrick@pichon.me>
2018-09-30 15:29:44 +0200

    commenting some variables

--- z_var.py
+++ z_var.py
@@ -6,11 +6,11 @@
 	Description: All global variables
 
 """
-FirmwareVersion = ''
-HeartbeatCount = 0
+FirmwareVersion = ''	# Firmware version , initialized at start by requesting the firmware version of Ziate
+HeartbeatCount = 0		
 transport = 'unknow'
 ReqRcv = ''
 ZigateConn = ''
-CrcCheck = 1
+CrcCheck = 1			# Enable of not the CrcCheck when receiving messages
 cmdInProgress = ''
-sendDelay = 0
+sendDelay = 0			# Enable or not a delay in send command 0- no delay -1 a delay of n seconds ( n is based on the number of commands in the ques
