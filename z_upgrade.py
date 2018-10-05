62210b60998f6aa78525045902c47edccf9b6f6b
Patrck Pichon <patrick@pichon.me>
2018-10-05 20:59:36 +0200

    fix some issues on the way to upgrade

--- z_upgrade.py
+++ z_upgrade.py
@@ -10,46 +10,54 @@ import z_var		  # Global variables
 def upgrade_v2( self, Devices ) :
 
 	nbdev = 0
-	upgradedone = False
+	upgradeflag = False
+
 	for x in Devices : # initialise listeofdevices avec les devices en bases domoticz
-		upgradeneeded = False
+		upgradeneedflag = False
 		
-		nbdev = nbdev + 1
 		Domoticz.Debug("Upgrading - oldDevicesOptions = " +str(Devices[x].Name) + " Options = "  + str(Devices[x].Options) )
 		newDevicesOptions=dict(Devices[x].Options)
 
 		if Devices[x].Options.get('TypeName') :
-			upgradedone = True
-			ugradeneeded = False
+			upgradeflag = True
+			upgradeneedflag = True
 			oldTypeName = newDevicesOptions['TypeName']
 
 			# Remove 'TypeName' and add 'ClusterType'
 			del newDevicesOptions['TypeName']
 			newDevicesOptions['ClusterType'] = oldTypeName
 
+		if  not Devices[x].Options.get('Zigate') :
+			Domoticz.Error("Upgrading no Zigate content found for " +str(Devices[x].Name) )
+			continue
+
 		ZigateV2 = eval(newDevicesOptions['Zigate'])
 		Domoticz.Debug("Upgrading - oldDevicesOptions['Zigate'] = " +str(Devices[x].Name) + " Zigate = "  + str(ZigateV2) )
 
 		if ZigateV2.get('Version') != "2" :
-			upgradedone = True
-			ugradeneeded = False
+			Domoticz.Debug("Upgrading to V2 " + str( Devices[x].DeviceID) )
+			upgradeflag = True
+			upgradeneedflag = True
 			ZigateV2['Version'] = "2"
 			newDevicesOptions['Zigate'] = ZigateV2
 
-		if upgradeneeded :
-			Domoticz.Log("Upgrading - newDevicesOptions = " +str(Devices[x].Name) + str(newDevicesOptions) )
-			Domoticz.Log("Upgrading " +str(Devices[x].Name) + " to version 2 with Options['Zigate']= " +str(ZigateV2) )
+		Domoticz.Debug("Upgrading upgradeflag = " +str(upgradeflag) + " upgradeneeded = " +str(upgradeneedflag) + " ndev = " +str(nbdev) )
+
+		if upgradeneedflag is True:
+			nbdev = nbdev + 1
+			Domoticz.Debug("Upgrading - newDevicesOptions = " +str(Devices[x].Name) + str(newDevicesOptions) )
+			Domoticz.Debug("Upgrading " +str(Devices[x].Name) + " to version 2 with Options['Zigate']= " +str(ZigateV2) )
 			nValue = Devices[x].nValue
 			sValue = Devices[x].sValue
 
 			Domoticz.Status("Upgrading " +str(Devices[x].Name) + " to version 2" )
 			Devices[x].Update(nValue=int(nValue), sValue=str(sValue), Options={}, SuppressTriggers=True )
 			Devices[x].Update(nValue=int(nValue), sValue=str(sValue), Options=newDevicesOptions, SuppressTriggers=True )
-
+			Domoticz.Debug("Upgrading - upgraded = " +str(Devices[x].Name) + " Options = "  + str(Devices[x].Options) )
 		else:
 			Domoticz.Debug("NOT Upgrading " +str(Devices[x].Name) + " Options ="  + str(Devices[x].Options) )
 
-	if upgradedone :
+	if upgradeflag is True :
 		Domoticz.Log("Upgrade of Zigate structure to V2 completed. " + str(nbdev) + " devices updated")
 	else:
 		Domoticz.Status("Zigate Structure V2")
@@ -58,7 +66,7 @@ def upgrade_v2( self, Devices ) :
 
 def upgrade_v3(self, Devices ):
 	nbdev = 0
-	upgradedone = False
+	upgradeflag = False
 	for x in Devices : 
 		if Devices[x].Options.get('Zigate') is None :
 			Domoticz.Error("upgrade_v3 aborted. Zigate structutre not found : " +str(Zigate['Version']) + " for " +str(x) )
@@ -121,9 +129,9 @@ def upgrade_v3(self, Devices ):
 				#Devices[y].Update(nValue=int(nValue), sValue=str(sValue), Image=Image, Used=Used, Color=Color, Description=Description, BatterLevel=BatteryLevel, SignalLevel=SignalLevel, SuppressTriggers=True )
 
 		nbdev = nbdev + 1
-		upgradedone = True
+		upgradeflag = True
 
-	if upgradedone :
+	if upgradeflag :
 		Domoticz.Status("Upgrade of Zigate structure to V3 completed. " + str(nbdev) + " devices updated")
 		
 
