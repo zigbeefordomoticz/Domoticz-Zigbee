4d70a0b3414da441889fa1ab458156102a778bf4
Patrck Pichon <patrick@pichon.me>
2018-10-22 18:35:01 +0200

    fix/deug

--- z_readClusters.py
+++ z_readClusters.py
@@ -124,27 +124,11 @@ def Cluster0702( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgA
 	# Smart Energy Metering
 	if MsgAttrID == "0000" : # Summation
 		bytelen = len(MsgClusterData)
-		Domoticz.Log("ReadCluster 0702 - MsgClusterData len = " +str(bytelen) )
-		if bytelen > 0 :
-			frames = bytelen/3
-			triads = struct.Struct('3s' * frames)
-			int4byte = struct.Struct('<i')
-			result = [int4byte.unpack('\0' + i)[0] >> 8 for i in triads.unpack(input_data)]
-			Domoticz.Log("ReadCluster - 0x0702 - Computed value = " + str(result) )
-			self.ListOfDevices[MsgSrcAddr]['Ep'][MsgSrcEp][MsgClusterId]=str(result)
-			z_domoticz.MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, str(result))
+		Domoticz.Log("ReadCluster 0702 - Attribute 0x0000 -MsgClusterData = " + str(MsgClusterData) + MsgClusterData len = " +str(bytelen) )
 
 	elif MsgAttrID == "1024" : # Instant Measurement 0x0400
 		bytelen = len(MsgClusterData)
-		Domoticz.Log("ReadCluster 0702 - MsgClusterData len = " +str(bytelen) )
-		if bytelen > 0 :
-			frames = bytelen/3
-			triads = struct.Struct('3s' * frames)
-			int4byte = struct.Struct('<i')
-			result = [int4byte.unpack('\0' + i)[0] >> 8 for i in triads.unpack(input_data)]
-			Domoticz.Log("ReadCluster - 0x0702 - Computed value = " + str(result) )
-			self.ListOfDevices[MsgSrcAddr]['Ep'][MsgSrcEp][MsgClusterId]=str(result)
-			z_domoticz.MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, str(result) )
+		Domoticz.Log("ReadCluster 0702 - Attribute 0x0400 - MsgClusterData = " + str(MsgClusterData) + MsgClusterData len = " +str(bytelen) )
 	else :
 		Domoticz.Log("ReadCluster - 0x0702 - NOT IMPLEMENTED YET - MsgAttrID = " +str(MsgAttrID) + " value = " + str(MsgClusterData) )
 		Domoticz.Log("ReadCluster - 0x0702 - NOT IMPLEMENTED YET - MsgAttType = " +str(MsgAttType) )
