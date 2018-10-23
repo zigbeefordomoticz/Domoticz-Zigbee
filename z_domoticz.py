2fe22b32c0d1068f7ab81c82e9828a819d5d4657
zaraki673 <azazel673@gmail.com>
2018-10-23 11:27:10 +0200

    fix fix reset

--- z_domoticz.py
+++ z_domoticz.py
@@ -605,11 +605,11 @@ def ResetDevice(self, Devices, Type, HbCount) :
 			if self.ListOfDevices[NWKID].get('RSSI') : 
 				SignalLevel = self.ListOfDevices[NWKID]['RSSI']
 			else :
-				SignalLevel = '100'
+				SignalLevel = 100
 			if self.ListOfDevices[NWKID].get('Battery') : 
 				BatteryLevel = self.ListOfDevices[NWKID]['Battery']
 			else :
-				BatteryLevel = '255'
+				BatteryLevel = 255
 
 			Domoticz.Debug("ResetDevice - Time delat since Last update : "+str( current - LUpdate) )
 		
