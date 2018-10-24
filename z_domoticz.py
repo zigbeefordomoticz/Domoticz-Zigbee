84c76af1499f9a34b4d5528abd9fee5fa25f66f6
zaraki673 <azazel673@gmail.com>
2018-10-24 19:29:09 +0200

    fix resetdevice

--- z_domoticz.py
+++ z_domoticz.py
@@ -605,7 +605,7 @@ def ResetDevice(self, Devices, Type, HbCount) :
 		
 			if (current - LUpdate)> 30 and Dtypename=="Motion":
 				Domoticz.Log("Last update of the devices " + str(x) + " was : " + str(LUpdate)  + " current is : " + str(current) + " this was : " + str(current-LUpdate) + " secondes ago")
-				UpdateDevice_v2(Devices, x, 0, "Off" ,BatteryLevel, SignalLevel, SuppTrigger=True)
+				UpdateDevice_v2(Devices, x, 0, "Off" ,BatteryLevel, SignalLevel, SuppTrigger_=True)
 	return
 			
 def UpdateDevice_v2(Devices, Unit, nValue, sValue, BatteryLvl, SignalLvl, Color_ = '', SuppTrigger_ =False, ForceUpdate_ = False ):
