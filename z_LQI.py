7ffa310b1a1005ff8d33dc04b4a747255baddbb2
Patrck Pichon <patrick@pichon.me>
2018-10-25 16:00:27 +0200

    continue implementation of LQI scan

--- z_LQI.py
+++ z_LQI.py
@@ -8,6 +8,31 @@
 """
 
 
+import Domoticz
+import z_output
+
+def LQIdiscovery( self ):
+	if not self.LQI :
+		self.LQI = {}
+
+	self.LQI['Index'] = 0
+
+	mgtLQIreq( self )
+
+def LQIcontinueScan( self ) :
+
+	for key in self.LQI :
+		if key == 'Index' : continue
+		if str(self.LQI[key]['Scanned']) == '0' :
+			Domoticz.Log("LQIcontinueScan - scan : " +str(key) )
+			self.LQI['Index'] += 1
+			self.LQI[key]['Scanned'] = 1
+			mgtLQIreq( self, key,  self.LQI['Index'] )
+
+	for key in self.ListOfDevices :
+		if key in self.LQI : continue
+		mgtLQIreq( self, key, self.LQI['Index'] )
+
 def mgtLQIreq( self, nwkid='0000', index=0 ):
 	'''
 	Send a Management LQI request 
@@ -16,9 +41,9 @@ def mgtLQIreq( self, nwkid='0000', index=0 ):
 	We start from 0000 Index 0
 	'''
 
-	datas = str(nwkid) + "{02]".format(index)
+	datas = str(nwkid) + "{:02n}".format(index)
 	Domoticz.Log("mgtLQIreq : from Nwkid : " +str(nwkid) + " index : "+str(index) )
-	sendZigateCmd("004E",datas, 2)	
+	z_output.sendZigateCmd("004E",datas, 2)	
 
 	return
 
@@ -46,36 +71,68 @@ def mgtLQIresp ( self , MsgData) :
 				bit 4-5 Relationship 		(0-Parent 1-Child 2-Sibling) 	
 				bit 6-7 Rx On When Idle status 			(1-On 0-Off)
 	'''
+	Domoticz.Debug("mgtLQIresp - MsgData = " +str(MsgData) )
 
 	SQN=MsgData[0:2]
 	Status=MsgData[2:4]
-	NeighbourTableEntrie=int(MsgData[4:6],16)
+	NeighbourTableEntries=int(MsgData[4:6],16)
 	NeighbourTableListCount=int(MsgData[6:8],16)
 	StartIndex=int(MsgData[8:10],16)
 	ListOfEntries=MsgData[10:len(MsgData)]
 
-	if not LQI :
-		LQI = {}
 
-	if int(NeighbourTableListCount,16) == 0 :
+	Domoticz.Log("mgtLQIresp - SQN = " +str(SQN) )
+	Domoticz.Log("mgtLQIresp - status = " +str(Status) )
+	Domoticz.Log("mgtLQIresp - NeighbourTableEntries = " +str(NeighbourTableEntries) )
+	Domoticz.Log("mgtLQIresp - NeighbourTableListCount = " +str(NeighbourTableListCount) )
+	Domoticz.Log("mgtLQIresp - StartIndex = " +str(StartIndex) )
+
+	if NeighbourTableListCount == 0 :
 		# No element in that list
 		Domoticz.Log("mgtLQIresp -  No element in that list ")
 		return
 
-	n = 10
-	while n < ((NeighbourTableListCount * 42) + 10 ):
+	n = 0
+	while n < ((NeighbourTableListCount * 42)):
 		_nwkid    = ListOfEntries[n:n+4]
 		_extPANID = ListOfEntries[n+4:n+20]
 		_ieee     = ListOfEntries[n+20:n+36]
-		_depth    = int(ListOfEntries[n+36:n+38], 16 )
-		_lnkqty   = int(ListOfEntries[n+38:n+40], 16 )
+		_depth    = ListOfEntries[n+36:n+38]
+		_lnkqty   = ListOfEntries[n+38:n+40]
 		_bitmap   = int(ListOfEntries[n+40:n+42],16)
-		_devicetype = ( _bitmap & 0x00000011 )
-        _permitjnt  = ( _bitmap >> 2 ) & 0x11
-        _relationshp= ( _bitmap >> 4 ) & 0x11
-        _rxonwhenidl= ( _bitmap >> 6 ) & 0x11
-		n = n + 42
-		Domoticz.Log("mgtLQIresp - Table["+str(NeighbourTableEntrie) + "] - " + " _nwkid = " +str(_nwkid) + " _extPANID = " +str(_extPANID) + " _ieee = " +str(_ieee) + " _depth = " +str(_depth) + " _lnkqty = " +str(_lnkqty) + " _devicetype = " +str(_devicetype) + " _permitjnt = " +str(_permitjnt) + " _relationshp = " +str(_relationshp) + " _rxonwhenidl = " +str(_rxonwhenidl)
+		Domoticz.Log("mgtLQIresp - _bitmap {0:b}".format(_bitmap) )
 
+		_devicetype   = _bitmap & 0b00000011
+		_permitjnt    = (_bitmap & 0b00001100) >> 2
+		_relationshp  = (_bitmap & 0b00110000) >> 4
+		_rxonwhenidl  = (_bitmap & 0b11000000) >> 6
+
+		if  _devicetype   == 0x00 : _devicetype = 'Coordinator'
+		elif  _devicetype == 0x01 : _devicetype = 'Router'
+		elif  _devicetype == 0x02 : _devicetype = 'End Device'
+
+		if _relationshp   == 0x01 : _relationshp = 'Parent'
+		elif _relationshp == 0x02 : _relationshp = 'Child'
+		elif _relationshp == 0x03 : _relationshp = 'Sibling'
+
+		if _permitjnt   == 0x00 : _permitjnt = 'Off'
+		elif _permitjnt == 0x01  : _permitjnt = 'On'
+		elif _permitjnt == 0x02  : _permitjnt = '??'
+
+		if _rxonwhenidl   == 0x00 : _rxonwhenidl = 'Rx-Off'
+		elif _rxonwhenidl == 0x01 : _rxonwhenidl = 'Rx-On'
+		n = n + 42
+		Domoticz.Log("mgtLQIresp - Table["+str(NeighbourTableEntries) + "] - " + " _nwkid = " +str(_nwkid) + " _extPANID = " +str(_extPANID) + " _ieee = " +str(_ieee) + " _depth = " +str(_depth) + " _lnkqty = " +str(_lnkqty) + " _devicetype = " +str(_devicetype) + " _permitjnt = " +str(_permitjnt) + " _relationshp = " +str(_relationshp) + " _rxonwhenidl = " +str(_rxonwhenidl) )
+
+		self.LQI[_nwkid] = {}
+		self.LQI[_nwkid]['_extPANID'] = _extPANID
+		self.LQI[_nwkid]['_ieee'] = _ieee
+		self.LQI[_nwkid]['_depth'] = _depth
+		self.LQI[_nwkid]['_lnkqty'] = _lnkqty
+		self.LQI[_nwkid]['_devicetype'] = _devicetype
+		self.LQI[_nwkid]['_permitjnt'] = _permitjnt
+		self.LQI[_nwkid]['_relationshp'] = _relationshp
+		self.LQI[_nwkid]['_rxonwhenidl'] = _rxonwhenidl
+		self.LQI[_nwkid]['Scanned'] = '0'
 
 	return
