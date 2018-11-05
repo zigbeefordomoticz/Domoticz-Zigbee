836ce1c5dab0f5481f1d56993932aeb06723df29
Patrck Pichon <patrick@pichon.me>
2018-11-05 22:02:45 +0100

    fix syntax error

--- z_readClusters.py
+++ z_readClusters.py
@@ -192,7 +192,7 @@ def Cluster0702( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgA
         return
 
     value = int(decodeAttribute( MsgAttType, MsgClusterData ))
-    Domoticz.Log("Cluster0702 - MsgAttrID: %s MsgAttType: %s decodedValue: %s" %s(MsgAttrID, MsgAttType, value))
+    Domoticz.Log("Cluster0702 - MsgAttrID: %s MsgAttType: %s decodedValue: %s" %(MsgAttrID, MsgAttType, value))
 
     if MsgAttrID == "0000": Domoticz.Debug("Cluster0702 - 0x0000 CURRENT_SUMMATION_DELIVERED")
     elif MsgAttrID == "0001": Domoticz.Debug("Cluster0702 - 0x0001 CURRENT_SUMMATION_RECEIVED ")
