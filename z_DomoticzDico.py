c08e7a4736611d8760da9221130126d4f9a3bb69
Patrck Pichon <patrick@pichon.me>
2018-12-11 21:32:12 +0100

    retreive AddjValue

--- z_DomoticzDico.py
+++ z_DomoticzDico.py
@@ -36,6 +36,30 @@ class DomoticzDictionnary:
         dbConn.close()
 
 
+class DeviceDictionnary:
+
+    def __init__(self, startupfolder, hardwareID ):
+        self.Devices = {}
+        self.dbConn = None
+        self.dbCursor = None
+        self.HardwareID = hardwareID
+        database = startupfolder + DOMOTICZ_DB
+
+        # Check if we have access to the database, if not Error and return
+        if not os.path.isfile( database ) :
+            return
+        self.dbConn = sqlite3.connect(database)
+        self.dbCursor = self.dbConn.cursor()
+
+
+    def retreiveAddjValue( self, ID):
+        """
+        Retreive the AddjValue of Device.ID
+        """
+
+        self.dbCursor.execute("SELECT AddjValue FROM DeviceStatus WHERE ID = '%s' and HardwareID = '%s'" %(ID, self.HardwareID))
+        value = self.dbCursor.fetchone()
+        return value[0]
 
 
 if __name__ == '__main__':
@@ -43,3 +67,7 @@ if __name__ == '__main__':
     domoDico = DomoticzDictionnary( "/var/lib/domoticz/")
     for key, value in  domoDico.preferences.items():
         print( "%s = %s" %(key,value))
+
+    tstdevice = DeviceDictionnary("/var/lib/domoticz/", "35")
+
+    print(tstdevice.AddjValue("620"))
