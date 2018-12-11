e91cf9704aa4577225938021fac2dc744a04b83e
Patrck Pichon <patrick@pichon.me>
2018-12-11 22:31:52 +0100

    add \n to LQI report in JSON format"

--- z_LQI.py
+++ z_LQI.py
@@ -115,6 +115,7 @@ def LQIcontinueScan(self):
             json_filename = _filename + ".json"
             with open( json_filename, 'at') as json_file:
                 json.dump( storeLQI, json_file)
+                json_file.write('\n')
         else:
             Domoticz.Error("Unable to get access to directory %s, please check PluginConf.txt" %(self.pluginconf.logRepo))
             self.pluginconf.logLQI = 0
