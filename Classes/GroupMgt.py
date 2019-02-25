e3cb6a569c764186c3888a751fcc5b4152279430
Patrick Pichon <patrick@pichon.me>
2019-02-25 09:07:46 +0100

    enable login for Group search or not

--- Classes/GroupMgt.py
+++ Classes/GroupMgt.py
@@ -813,15 +813,15 @@ class GroupsManagement(object):
             Domoticz.Log("Group Management - Init phase")
             self.StartupPhase = 'discovery'
             if os.path.isfile( self.groupListFileName ) :
-                Domoticz.Debug("GroupList.pck exists")
+                Domoticz.Log("GroupList.pck exists")
                 last_update_GroupList = modification_date( self.groupListFileName )
-                Domoticz.Debug("Last Update of GroupList: %s" %last_update_GroupList)
+                Domoticz.Log("Last Update of GroupList: %s" %last_update_GroupList)
 
                 if self.groupsConfigFilename:
                     if os.path.isfile( self.groupsConfigFilename ):
-                        Domoticz.Debug("Config file exists")
+                        Domoticz.Log("Config file exists")
                         last_update_ConfigFile = modification_date( self.groupsConfigFilename )
-                        Domoticz.Debug("Last Update of Config File: %s" %last_update_ConfigFile)
+                        Domoticz.Log("Last Update of Config File: %s" %last_update_ConfigFile)
                         if last_update_GroupList > last_update_ConfigFile :
                             # GroupList is newer , just reload the file and exit
                             Domoticz.Status("No update of Groups needed")
