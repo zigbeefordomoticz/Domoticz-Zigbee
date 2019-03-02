87a5da90f893f97e789787fd962353a1e602f1b1
Patrick Pichon <patrick@pichon.me>
2019-03-02 09:19:08 +0100

    cosmetic fix to avoid // instead of / in pathname

--- Classes/PluginConf.py
+++ Classes/PluginConf.py
@@ -56,10 +56,10 @@ class PluginConf:
         # Plugin Directories
         self.pluginHome = homedir
         self.homedirectory = homedir
-        self.pluginData = self.pluginHome + '/Data/'
-        self.pluginZData = self.pluginHome + '/Zdatas/'
-        self.pluginConfig = self.pluginHome + '/Conf/'
-        self.pluginWWW = self.pluginHome + '/www/'
+        self.pluginData = self.pluginHome + 'Data/'
+        self.pluginZData = self.pluginHome + 'Zdatas/'
+        self.pluginConfig = self.pluginHome + 'Conf/'
+        self.pluginWWW = self.pluginHome + 'www/'
         #self.pluginReports = self.pluginWWW + 'zigate/reports/'
         self.pluginReports = self.pluginHome + 'www/zigate/reports/'
 
