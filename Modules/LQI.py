a51cb42735db3295b02d6df033a7cfa81f368bd3
Patrick Pichon <patrick@pichon.me>
2019-05-17 23:09:18 +0200

    remove .txt extension and keep .json

--- Modules/LQI.py
+++ Modules/LQI.py
@@ -103,20 +103,13 @@ def LQIcontinueScan(self, Devices):
                             %(src, child, self.LQI[src][child]['_relationshp'], self.LQI[src][child]['_devicetype'], self.LQI[src][child]['_depth'], self.LQI[src][child]['_rxonwhenidl']))
 
         # Write the report onto file
-        _filename = self.pluginconf.pluginReports + 'LQI_reports-' + '%02d' %self.HardwareID + '.txt'
+        _filename = self.pluginconf.pluginReports + 'LQI_reports-' + '%02d' %self.HardwareID + '.json'
         storeLQI = {}
         storeLQI[int(time.time())] = self.LQI
 
         self.pluginconf.logLQI = 0
         if os.path.isdir( self.pluginconf.pluginReports ):
-            #Domoticz.Status("LQI report save on " +str(_filename))
-            #with open(_filename , 'at') as file:
-            #    for key in storeLQI:
-            #        file.write(str(key) + ": " + str(storeLQI[key]) + "\n")
-            #self.pluginconf.logLQI = 0
-
-            json_filename = _filename + ".json"
-            with open( json_filename, 'at') as json_file:
+            with open( _filename, 'at') as json_file:
                 json_file.write('\n')
                 json.dump( storeLQI, json_file)
             self.adminWidgets.updateNotificationWidget( Devices, 'A new LQI report is available')
