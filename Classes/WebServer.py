c969acebb7dcc2c129c5424695eed493c1a7fbc5
Patrick Pichon <patrick@pichon.me>
2019-05-18 10:00:13 +0200

    Permit-to-join into PermitToJoin

--- Classes/WebServer.py
+++ Classes/WebServer.py
@@ -339,7 +339,7 @@ class WebServer(object):
         _response["Headers"]["Content-Type"] = "application/json; charset=utf-8"
 
         if verb == 'GET':
-            _response["Data"] = '{"permit-to-join":"254"}'
+            _response["Data"] = '{"PermitToJoin":"254"}'
 
         elif verb == 'PUT':
             _response["Data"] = None
