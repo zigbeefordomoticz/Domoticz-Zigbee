a75704cafd1aefd7ba4156b9f85978d9722972df
deennoo <deennoo33@gmail.com>
2019-06-05 21:31:44 +0200

    Correction
    Missing : 
    gpio mode 0 out
    
    To enter in flash and run mode

--- Tools/pi-zigate.sh
+++ Tools/pi-zigate.sh
@@ -8,17 +8,19 @@ fi
 if [ $1 == "flash" ]
 then
 	echo "Switching PI-Zigate to Flash mode"
+	gpio mode 0 out
 	gpio mode 2 out
-	gpio write 2 0
-	gpio mode 0 down
-	gpio mode 0 up
+	gpio write 2 0 
+	gpio write 0 0
+	gpio write 0 1
 fi
 
 if [ $1 == "run" ]
 then
 	echo "Switching PI-Zigate to Run mode"
+	gpio mode 0 out
 	gpio mode 2 out
-	gpio write 2 1
-	gpio mode 0 down
-	gpio mode 0 up
+	gpio write 2 1 
+	gpio write 0 0
+	gpio write 0 1
 fi
