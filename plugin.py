e6bfdc7dc82259b674fbeeaa2cd4121d24344e75
Patrick Pichon <patrick@pichon.me>
2019-06-13 21:43:25 +0200

    4.3.2

--- plugin.py
+++ plugin.py
@@ -5,7 +5,7 @@
 #
 
 """
-<plugin key="Zigate" name="Zigate plugin" author="zaraki673 & pipiche38" version="4.3.1" wikilink="https://www.domoticz.com/wiki/Zigate" externallink="https://github.com/sasu-drooz/Domoticz-Zigate/wiki">
+<plugin key="Zigate" name="Zigate plugin" author="zaraki673 & pipiche38" version="4.3.2" wikilink="https://www.domoticz.com/wiki/Zigate" externallink="https://github.com/sasu-drooz/Domoticz-Zigate/wiki">
     <description>
         <h2> Plugin Zigate for Domoticz </h2><br/>
     <h3> Short description </h3>
@@ -163,7 +163,7 @@ class BasePlugin:
 
     def onStart(self):
 
-        Domoticz.Status("Zigate plugin 4.3.1 started")
+        Domoticz.Status("Zigate plugin 4.3.2 started")
 
         Domoticz.Log("Debug: %s" %int(Parameters["Mode6"]))
         if Parameters["Mode6"] != "0":
