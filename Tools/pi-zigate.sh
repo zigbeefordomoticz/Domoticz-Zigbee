4e3253002fdcdeaa778f06444047ed6177c1c45c
Pipiche <patrick@pichon.me>
2019-07-02 19:25:06 +0100

    Add Verification/reading of pin0 and pin2

--- Tools/pi-zigate.sh
+++ Tools/pi-zigate.sh
@@ -5,9 +5,27 @@ then
 	echo "Usage: pi-zigate.sh flash | run " 
 	exit 0
 fi
+echo "Verif GPIO ..."
+io0=`gpio read 0`
+io2=`gpio read 2`
+
+if [ "$io0" -eq "1" ]
+then
+	echo "+ GPIO 0 (RESET) $io0 --> OK"
+else
+	echo "+ GPIO 0 (RESET) $io0 --> KO"
+fi
+if [ "$io2" -eq "1" ]
+then
+	echo "+ GPIO 2 (FLASH) $io2 --> OK"
+else
+	echo "+ GPIO 2 (FLASH) $io2 --> KO"
+fi
+
 if [ $1 == "flash" ]
 then
 	echo "Switching PI-Zigate to Flash mode"
+
 	gpio mode 0 out
 	gpio mode 2 out
 	gpio write 2 0 
