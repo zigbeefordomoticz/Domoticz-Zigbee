4e0759eb955ccfbe95747ed26a2c9cf6433e9436
Patrick Pichon <patrick@pichon.me>
2019-07-07 20:49:52 +0200

    update version

--- plugin.py
+++ plugin.py
@@ -180,7 +180,7 @@ class BasePlugin:
 
         self.pluginParameters = dict(Parameters)
         self.pluginParameters['PluginBranch'] = 'beta'
-        self.pluginParameters['PluginVersion'] = '4.4.4'
+        self.pluginParameters['PluginVersion'] = '4.4.5'
         self.pluginParameters['TimeStamp'] = 0
         self.pluginParameters['available'] =  None
         self.pluginParameters['available-firmMajor'] =  None
