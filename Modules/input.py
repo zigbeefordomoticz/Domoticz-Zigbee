b67faf97ef49414c064c53b28bc2d2dd91b79464
Patrick Pichon <patrick@pichon.me>
2019-07-30 17:20:32 +0200

    move a Log message to Debug

--- Modules/input.py
+++ Modules/input.py
@@ -1567,7 +1567,7 @@ def Decode8702(self, Devices, MsgData, MsgRSSI) : # Reception APS Data confirm f
         Domoticz.Log("Decode8702 - Unknown Address %s : (%s,%s)" %( MsgDataDestAddr, NWKID, IEEE ))
         return
     
-    Domoticz.Log("Decode8702 - IEEE: %s Nwkid: %s Status: %s" %(IEEE, NWKID, MsgDataStatus))
+    loggingMessages( self, 'Debug', "Decode8702 - IEEE: %s Nwkid: %s Status: %s" %(IEEE, NWKID, MsgDataStatus), NWKID)
     if self.APS:
         self.APS.processAPSFailure(NWKID, IEEE, MsgDataStatus)
 
