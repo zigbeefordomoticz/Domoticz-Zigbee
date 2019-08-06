4fb4af52338c51b4a1eb7f4ab91b71a8e7da6ccd
Patrick Pichon <patrick@pichon.me>
2019-08-06 21:40:45 +0200

    4.5.2

--- plugin.py
+++ plugin.py
@@ -184,7 +184,7 @@ class BasePlugin:
 
         self.pluginParameters = dict(Parameters)
         self.pluginParameters['PluginBranch'] = 'stable'
-        self.pluginParameters['PluginVersion'] = '4.5.1'
+        self.pluginParameters['PluginVersion'] = '4.5.2'
         self.pluginParameters['TimeStamp'] = 0
         self.pluginParameters['available'] =  None
         self.pluginParameters['available-firmMajor'] =  None
