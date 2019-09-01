6676c56fb3a6d2ce14fd6c9969b8971d86168f38
Patrick Pichon <patrick@pichon.me>
2019-09-01 22:59:15 +0200

    https://github.com/pipiche38/Domoticz-Zigate/issues/470

--- Modules/readClusters.py
+++ Modules/readClusters.py
@@ -475,7 +475,7 @@ def Cluster0001( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgA
         self.ListOfDevices[MsgSrcAddr]['Ep'][MsgSrcEp][MsgClusterId] = newValue
         MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId,str(value))
         loggingCluster( self, 'Log', "readCluster 0001 - %s General Voltage: %s V " %(MsgSrcAddr, value) , MsgSrcAddr)
-        if self.ListOfDevice[ MsgSrcAddr]['MacCapa'] == '8e':
+        if self.ListOfDevices[ MsgSrcAddr]['MacCapa'] == '8e':
             # This should reflect the main voltage.
             return
 
