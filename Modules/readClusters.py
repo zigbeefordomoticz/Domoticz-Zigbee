7f0e2ba9f3811730ac98a1111dcc22e1e1bf2457
Patrick Pichon <patrick@pichon.me>
2019-11-22 14:30:34 +0100

    remove unnecessary ping to battery Xiaomi devices

--- Modules/readClusters.py
+++ Modules/readClusters.py
@@ -23,7 +23,7 @@ from math import atan, sqrt, pi
 from Modules.zigateConsts import LEGRAND_REMOTE_SHUTTER, LEGRAND_REMOTE_SWITCHS, LEGRAND_REMOTES
 from Modules.domoticz import MajDomoDevice, lastSeenUpdate, timedOutDevice
 from Modules.tools import DeviceExist, getEPforClusterType, is_hex, loggingCluster
-from Modules.output import ReadAttributeRequest_Ack, xiaomi_leave
+from Modules.output import  xiaomi_leave
 
 def retreive4Tag(tag,chain):
     c = str.find(chain,tag) + 4
@@ -414,8 +414,6 @@ def Cluster0000( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgA
         if self.ListOfDevices[MsgSrcAddr]['Status'] != "inDB":  # xiaomi battery lvl
             Domoticz.Error("ReadCluster - %s - %s/%s Attribut %s received while device not inDB" %(MsgClusterId, MsgSrcAddr, MsgSrcEp, MsgAttrID))
             return
-
-        ReadAttributeRequest_Ack(self, MsgSrcAddr)         # Ping Xiaomi devices
 
         loggingCluster( self, 'Debug', "ReadCluster - %s %s Saddr: %s ClusterData: %s" %(MsgClusterId, MsgAttrID, MsgSrcAddr, MsgClusterData), MsgSrcAddr)
         self.ListOfDevices[MsgSrcAddr]['Ep'][MsgSrcEp][MsgClusterId][MsgAttrID] = MsgClusterData
