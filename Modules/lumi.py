6f5db4a30c42b89e9b88e2eebf06869f0cf07e5b
Patrick Pichon <patrick@pichon.me>
2020-04-14 20:18:12 +0200

    update

---
+++ Modules/lumi.py
@@ -0,0 +1,22 @@
+
+
+import Domoticz
+
+from Modules.output import write_attribute
+
+def enableOppleSwitch( self, nwkid ):
+
+    if nwkid not in self.ListOfDevices:
+        return
+
+    manuf_id = '115F'
+    manuf_spec = "01"
+    cluster_id = 'FCC0'
+    Hattribute = '0009'
+    data_type = '20'
+    Hdata = '01'
+
+
+    Domoticz.Log( "Write Attributes LUMI Magic Word Nwkid: %s" %nwkid)
+    write_attribute( self, nwkid, "01", '01', cluster_id, manuf_id, manuf_spec, Hattribute, data_type, Hdata)
+
