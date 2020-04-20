1af15f438e20c2c19cd544a6e61c33067a53f0f1
Patrick Pichon <patrick@pichon.me>
2020-04-20 11:27:40 +0200

    increment version

--- plugin.py
+++ plugin.py
@@ -196,7 +196,7 @@ class BasePlugin:
 
         self.pluginParameters = dict(Parameters)
         self.pluginParameters['PluginBranch'] = 'beta'
-        self.pluginParameters['PluginVersion'] = '4.8.016'
+        self.pluginParameters['PluginVersion'] = '4.8.017'
         self.pluginParameters['TimeStamp'] = 0
         self.pluginParameters['available'] =  None
         self.pluginParameters['available-firmMajor'] =  None
