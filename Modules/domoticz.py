781dbc4b3b68462a6b4b2976dee39b8eacb2470e
Pipiche <patrick@pichon.me>
2020-05-06 16:05:28 +0200

    Update domoticz.py

--- Modules/domoticz.py
+++ Modules/domoticz.py
@@ -629,7 +629,8 @@ def MajDomoDevice(self, Devices, NWKID, Ep, clusterID, value, Attribute_='', Col
     ClusterTypeList = []
     for iterEp in self.ListOfDevices[NWKID]['Ep']:  
         if 'ClusterType' in self.ListOfDevices[NWKID]['Ep'][iterEp]:
-            for iterEp, WidgetId, WidgetType in self.ListOfDevices[NWKID]['Ep'][iterEp]['ClusterType']:
+            for WidgetId  in self.ListOfDevices[NWKID]['Ep'][iterEp]['ClusterType']:
+                WidgetType = self.ListOfDevices[NWKID]['Ep'][iterEp]['ClusterType'][WidgetId]
                 ClusterTypeList.append(  ( iterEp, WidgetId, WidgetType )  )
     loggingWidget( self, 'Debug', "------> WidgetList: %s" %str(ClusterTypeList), NWKID)
 
