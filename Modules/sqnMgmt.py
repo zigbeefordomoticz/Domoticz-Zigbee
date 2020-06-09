1ad11d00afaf923f7ace6f8b6c856325723d29ee
badzz <mbadaire@gmail.com>
2020-06-09 20:45:06 +0200

    update sqnMgmt
    allow getting i_sqn based on different type of e_eqn

--- Modules/sqnMgmt.py
+++ Modules/sqnMgmt.py
@@ -19,9 +19,12 @@ from itertools import filterfalse
 # list of [0:i_sqn, 1:e_sqn, 2:timestamp_query_in_seconds, 3:function_answer_status, 4:ack_status, 5:response_status]
 
 I_SQN = 0
-E_SQN = 1
-TIMESTAMP = 2
+E_SQN_APS = 1
+E_SQN_ZCL = 2
+TIMESTAMP = 3
 TIMEOUT = 800 #seconds
+
+NO_SQN = -1
 
 
 def sqn_init_stack(self):
@@ -41,12 +44,12 @@ def sqn_generate_new_internal_sqn (self): # to be called in zigatecmd
 
     now = int(time.time())
 
-    self.sqn_stack.append ([i_sqn, -1, now])
+    self.sqn_stack.append ([i_sqn, NO_SQN, NO_SQN, now])
     self.loggingSend(  'Debug',"sqnMgmt generate_new_internal_sqn %s" %i_sqn)
     
     return i_sqn
 
-def sqn_add_external_sqn (self, i_sqn, e_sqn, e_sqnZcl): # to be called in Transport when receiving 0x8000
+def sqn_add_external_sqn (self, i_sqn, e_sqnAPS, e_sqnZcl): # to be called in Transport when receiving 0x8000
 
   #  Domoticz.Error ("sqnMgmt add_external_sqn %s %s" %(e_sqn, self.sqn_stack))
 
@@ -54,30 +57,30 @@ def sqn_add_external_sqn (self, i_sqn, e_sqn, e_sqnZcl): # to be called in Trans
         if sqn_tuple[ I_SQN ] == i_sqn:
         #if sqn_tuple[E_SQN]  == -1 :
             self.loggingSend(  'Debug',"sqn_add_external_sqn -> %s %s" %(i_sqn, sqn_tuple[ I_SQN ] ))
-            sqn_tuple[E_SQN] = e_sqn
+            sqn_tuple[E_SQN_APS] = e_sqnAPS
+            sqn_tuple[E_SQN_ZCL] = e_sqnZcl
             return
     self.loggingSend(  'Error',"sqnMgmt add_external_sqn could not find i_sqn corresponding %s %s" %(e_sqn, self.sqn_stack))
 
-def sqn_get_tuple (self, e_sqn):
+def sqn_get_tuple (self, e_sqn, sqn_type = E_SQN_APS):
 
     for sqn_tuple in self.sqn_stack:
-        if sqn_tuple[E_SQN] == e_sqn:
+        if sqn_tuple[sqn_type] == e_sqn:
             return sqn_tuple
     return None
 
-def sqn_get_internal_sqn (self, e_sqn):
-
-    sqn_tuple = sqn_get_tuple (self, e_sqn)
+def sqn_get_internal_sqn (self, e_sqn, sqn_type = E_SQN_APS):
+    sqn_tuple = sqn_get_tuple (self, e_sqn, sqn_type)
     if sqn_tuple is not None:
-        self.loggingSend(  'Debug',"sqnMgmt sqn_get_internal found i_sqn %s" %(sqn_tuple[0]))
+        self.loggingSend(  'Debug',"sqnMgmt sqn_get_internal found i_sqn %s" %(sqn_tuple[I_SQN]))
         return sqn_tuple[I_SQN]
     else:
         Domoticz.Error ("sqnMgmt sqn_get_internal not found e_sqn %s" %(e_sqn))
         return None
 
-def sqn_cleanup (self):
+def sqn_cleanup (self, timeout = TIMEOUT):
     now = int(time.time())
-    newstack  = filterfalse(lambda sqn_tuple: now > (sqn_tuple[TIMESTAMP] + TIMEOUT), self.sqn_stack )
+    newstack  = filterfalse(lambda sqn_tuple: now > (sqn_tuple[TIMESTAMP] + timeout), self.sqn_stack )
     self.sqn_stack = list(newstack)
 
 def sqn_delete (self, i_sqn):
@@ -90,6 +93,7 @@ def sqn_reset (self,i_sqn):
 
     for sqn_tuple in self.sqn_stack:
         if sqn_tuple[I_SQN] == i_sqn:
-            sqn_tuple[E_SQN] = -1
+            sqn_tuple[E_SQN_APS] = NO_SQN
+            sqn_tuple[E_SQN_ZCL] = NO_SQN
             sqn_tuple[TIMESTAMP] = now
 
