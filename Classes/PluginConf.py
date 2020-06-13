b92571b683c64acecb602980283d1c884f29ae9c
Pipiche <patrick@pichon.me>
2020-06-13 08:28:34 +0200

    Introduce a specific parameter to enable Livolo
    'Livolo' parameter to enable Livolo pairing

--- Classes/PluginConf.py
+++ Classes/PluginConf.py
@@ -227,6 +227,7 @@ SETTINGS = {
 
     #Others
     'Others': { 'Order': 15, 'param': {
+        'Livolo': {'type':'bool', 'default':0, 'current': None, 'restart':False , 'hidden':False, 'Advanced':False},
         'profaluxOrientBSO': {'type':'int', 'default':45, 'current': None, 'restart':False , 'hidden':False, 'Advanced':False},
         'alarmDuration': {'type':'int', 'default':1, 'current': None, 'restart':False , 'hidden':False, 'Advanced':False},
         'numTopologyReports': { 'type':'int', 'default':4 , 'current': None, 'restart':False , 'hidden':False, 'Advanced':False},
