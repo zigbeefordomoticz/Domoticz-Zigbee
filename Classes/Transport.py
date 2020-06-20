6b6085976140c98cbb172d00eb8cdae45cbd6e79
Patrick Pichon <patrick@pichon.me>
2020-06-20 17:02:17 +0200

    enable sqn check

--- Classes/Transport.py
+++ Classes/Transport.py
@@ -1084,7 +1084,6 @@ def process_other_type_of_message(self, MsgType, MsgSqn = None, MsgNwkId=None, M
             self.loggingSend( 'Error', " --  -- - > Expecting: %04x Receiving: %s" %(expResponse,MsgType ))
             self.loggingSend( 'Error', " --  -- - > Expected IntSqn: %s Received ISqn: %s ESqn: %s" %(InternalSqn, isqn, MsgSqn))
             self.loggingSend( 'Error', " --  -- - > Expecting: %s %s %s receiving %s %s %s" %( expNwkId, expEp, expCluster, MsgNwkId, MsgEp, MsgClusterId))
-
             return None
  
         ready_to_send_if_needed( self )
