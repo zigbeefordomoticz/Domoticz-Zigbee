04612b032be061b7139383d08bc1e1ff62879e16
Patrick Pichon <patrick@pichon.me>
2020-07-26 14:16:19 +0200

    revert previous change

--- Modules/widgets.py
+++ Modules/widgets.py
@@ -26,8 +26,8 @@ SWITCH_LVL_MATRIX = {
 
     "DoorLock":  {
         # Doorlock widget seems to behave inverted
-        "01": ( 0, "Closed" ),
-        "00": ( 1, "Open" ),
+        "00": ( 0, "Closed" ),
+        "01": ( 1, "Open" ),
         "ForceUpdate": False },
 
     "Smoke": { 
