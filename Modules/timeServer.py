c634377b7a4bb280bcc07a0e4e91ee5d0e2bc9e4
Patrick Pichon <patrick@pichon.me>
2020-09-03 16:41:44 +0200

    return UTC time for 0x0007 on cluster 0x000a

--- Modules/timeServer.py
+++ Modules/timeServer.py
@@ -7,23 +7,32 @@
 import Domoticz
 
 from Modules.basicInputs import read_attribute_response
-
+from datetime import datetime
 
 
 def timeserver_read_attribute_request( self, sqn, nwkid, ep, cluster, manuf_spec, manuf_code , attribute):
     Domoticz.Log("timeserver_read_attribute_request %s/%s Cluster: %s Attribute: %s" %(nwkid, ep, cluster,attribute))
 
     data_type = value = None
-    if attribute == '0000': # Time (should be probded by ZiGate)
-        pass
-    elif attribute == '0002': # Timezone
+    status = '86'
 
+    if attribute == '0000': # Time (should be probded by ZiGate)
+        EPOCTime = datetime(2000,1,1)
+        UTCTime = int((datetime.now() - EPOCTime).total_seconds())
+        value = "%08x" %UTCTime
+        data_type = '23'
+        status = '00'
+
+    elif attribute == '0002': # Timezone
         #int32
         pass
+
     elif attribute == '0007': # LocalTime
         #uint32
-        pass
-
-    status = '86'
+        EPOCTime = datetime(2000,1,1)
+        UTCTime = int((datetime.now() - EPOCTime).total_seconds())
+        value = "%08x" %UTCTime
+        data_type = '23'
+        status = '00'
 
     read_attribute_response( self, nwkid, ep, sqn, cluster, status, data_type, attribute, value, manuf_code='0000')
