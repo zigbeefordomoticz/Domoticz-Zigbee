806269ba0d6a3b7d65e0c8f0abc2a28208932956
Patrick Pichon <patrick@pichon.me>
2020-09-04 12:37:47 +0200

    Time Server implementation

--- Modules/timeServer.py
+++ Modules/timeServer.py
@@ -37,7 +37,7 @@ def timeserver_read_attribute_request( self, sqn, nwkid, ep, cluster, manuf_spec
         EPOCTime = datetime(2000,1,1)
         UTCTime = int((datetime.now() - EPOCTime).total_seconds())
         value = "%08x" %UTCTime
-        data_type = 'e2'
+        data_type = '23'
         status = '00'
 
     read_attribute_response( self, nwkid, ep, sqn, cluster, status, data_type, attribute, value, manuf_code='0000')
