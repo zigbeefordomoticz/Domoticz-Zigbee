e8eafa7141b31189a35a45b1b1a8ec7ce5faf16f
Patrick Pichon <patrick@pichon.me>
2020-09-04 12:38:36 +0200

    Time Server implementation

--- Modules/timeServer.py
+++ Modules/timeServer.py
@@ -21,23 +21,21 @@ def timeserver_read_attribute_request( self, sqn, nwkid, ep, cluster, manuf_spec
         EPOCTime = datetime(2000,1,1)
         UTCTime = int((datetime.now() - EPOCTime).total_seconds())
         value = "%08x" %UTCTime
-        data_type = 'e2'
+        data_type = 'e2' # UTC Type
         status = '00'
 
     elif attribute == '0002': # Timezone
-        #int32
         value = "%08x" %0x00000000
-        data_type = '23'
+        data_type = '23' # unint32
         status = '00'
 
 
     elif attribute == '0007': # LocalTime
         Domoticz.Log("-->Local Time: %s" %datetime.now())
-        #uint32
         EPOCTime = datetime(2000,1,1)
         UTCTime = int((datetime.now() - EPOCTime).total_seconds())
         value = "%08x" %UTCTime
-        data_type = '23'
+        data_type = '23' # uint32
         status = '00'
 
     read_attribute_response( self, nwkid, ep, sqn, cluster, status, data_type, attribute, value, manuf_code='0000')
