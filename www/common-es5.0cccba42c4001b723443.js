4d3f0b88c7e7216bae6777408f70e0034af542cf
Patrick Pichon <patrick@pichon.me>
2020-12-06 15:15:31 +0100

    Spanish translation for the Web UI

---
+++ www/common-es5.0cccba42c4001b723443.js
@@ -0,0 +1,1 @@
+!function(){function n(n,t){if(!(n instanceof t))throw new TypeError("Cannot call a class as a function")}(window.webpackJsonp=window.webpackJsonp||[]).push([[1],{D0Ju:function(t,o,i){"use strict";i.d(o,"b",function(){return c}),i.d(o,"a",function(){return u});var c=function t(){n(this,t)},u=function t(){n(this,t)}}}])}();
