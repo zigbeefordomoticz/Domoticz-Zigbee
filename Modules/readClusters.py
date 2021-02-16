4c94bc8c7aab8d3e820a5e5baade89a1c9e0e623
Patrick Pichon <patrick@pichon.me>
2021-02-16 12:30:27 +0100

    decoding correctly the Calibration attribut

--- Modules/readClusters.py
+++ Modules/readClusters.py
@@ -1516,7 +1516,13 @@ def Cluster0201( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgA
 
     elif MsgAttrID == '0010':   # Calibration / Adjustement
         self.log.logging( "Cluster", 'Debug', "ReadCluster - 0201 - Calibration: %s" %value, MsgSrcAddr)
+        if value not in range( 0x00, 0x19 ):
+            # We are in Negative value. 0xE7 = -25 0xff = -01 )
+            value = -( 0xff + 1 - value )
+        value = round ( value / 10, 2 )
+
         checkAndStoreAttributeValue( self, MsgSrcAddr, MsgSrcEp,MsgClusterId, MsgAttrID,  value )
+
         if 'Model' in self.ListOfDevices[MsgSrcAddr] and self.ListOfDevices[MsgSrcAddr]['Model'] == 'EH-ZB-VACT':
             self.ListOfDevices[MsgSrcAddr]['Schneider']['Calibration'] = value
 
