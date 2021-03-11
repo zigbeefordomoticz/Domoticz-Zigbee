8e59e5c0ee9339fb429d979329a1bc89fb7f6793
Patrick Pichon <patrick@pichon.me>
2021-03-11 19:20:18 +0100

    replace self.logging by Domoticz.Log as we do not have the self in this function

--- Classes/WebServer/tools.py
+++ Classes/WebServer/tools.py
@@ -19,14 +19,14 @@ def DumpHTTPResponseToLog(httpDict):
     if not DEBUG_HTTP:
         return
     if isinstance(httpDict, dict):
-        self.logging( 'Log', "HTTP Details ("+str(len(httpDict))+"):")
+        Domoticz.Log( "HTTP Details ("+str(len(httpDict))+"):")
         for x in httpDict:
             if isinstance(httpDict[x], dict):
-                self.logging( 'Log', "--->'"+x+" ("+str(len(httpDict[x]))+"):")
+                Domoticz.Log(  "--->'"+x+" ("+str(len(httpDict[x]))+"):")
                 for y in httpDict[x]:
-                    self.logging( 'Log', "------->'" + y + "':'" + str(httpDict[x][y]) + "'")
+                    Domoticz.Log(  "------->'" + y + "':'" + str(httpDict[x][y]) + "'")
             else:
                 if x == 'Data':
-                    self.logging( 'Log', "--->'%s':'%.40s'" %(x, str(httpDict[x])))
+                    Domoticz.Log(  "--->'%s':'%.40s'" %(x, str(httpDict[x])))
                 else:
-                    self.logging( 'Log', "--->'" + x + "':'" + str(httpDict[x]) + "'")
+                    Domoticz.Log(  "--->'" + x + "':'" + str(httpDict[x]) + "'")
