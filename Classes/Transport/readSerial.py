701fbedbcfa0fa86337519ffd3af300fd6d8a6f4
Patrick Pichon <patrick@pichon.me>
2021-04-26 15:01:30 +0200

    come back to a blocking read(1) without using in_waiting

--- Classes/Transport/readSerial.py
+++ Classes/Transport/readSerial.py
@@ -13,12 +13,16 @@ from Classes.Transport.readDecoder import decode_and_split_message
 
 # Manage Serial Line
 def open_serial( self ):
-
     try:
+        if self._connection:
+            self._connection.close()
+            del self._connection
+            self._connection = None
+
         self._connection = serial.Serial(self._serialPort, baudrate = 115200, rtscts = False, dsrdtr = False, timeout = None)
         if self._transp in ('DIN', 'V2' ):
             self._connection.rtscts = True
-        time.sleep(0.1)     # wait fro 100 ms for pyserial port to actually be ready
+        time.sleep(0.5)     # wait fro 100 ms for pyserial port to actually be ready
 
     except serial.SerialException as e:
         self.logging_receive('Error',"Cannot open Zigate port %s error: %s" %(self._serialPort, e))
@@ -36,9 +40,11 @@ def close_serial( self):
     self.logging_receive( 'Status', "ZigateTransport: Serial Connection closing: %s" %self._connection)
     try:
         self._connection.close()
+        del self._connection
+        self._connection = None
     except:
         pass
-    time.sleep(1.0)
+    time.sleep(0.5)
 
 def serial_read_from_zigate( self ):
 
@@ -46,27 +52,29 @@ def serial_read_from_zigate( self ):
     serial_reset_line_in(self)
     while self.running:
         # We loop until self.running is set to False, 
-        # which indicate plugin shutdown   
+        # which indicate plugin shutdown  
+        if not (self._connection and self._connection.is_open):
+            time.sleep(0.5)
+            continue
+        self.logging_receive( 'Debug', "serial_read_from_zigate - reading %s bytes" %1)
         data = None
         try:
             if self._connection:
-                nb_inwaiting = self._connection.in_waiting
-                self.logging_receive( 'Debug', "serial_read_from_zigate - reading %s bytes" %nb_inwaiting)
-                if nb_inwaiting:
-                    data = self._connection.read( nb_inwaiting)  # Blocking Read
-                else:
-                    data = self._connection.read( 1)  # Blocking Read
+                data = self._connection.read( 1)  # Blocking Read
 
         except serial.SerialException as e:
             self.logging_receive('Error',"serial_read_from_zigate - error while reading %s" %(e))
             data = None
+            self._connection= None
+            break
 
         except Exception as e:
+            # probably some I/O problem such as disconnected USB serial
+            # adapters -> exit
             self.logging_receive('Error',"Error while receiving a ZiGate command: %s" %e)
             handle_thread_error( self, e, 0, 0, data)
-            # Most likely the best would be to close the port and re-open it
-            close_serial(self)
-            open_serial( self )
+            self._connection = None
+            break
 
         if data:
             decode_and_split_message(self, data)
