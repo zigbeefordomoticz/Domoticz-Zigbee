b9bbdc30270b1de7428fff890d60d44e602ce335
deennoo <deennoo33@gmail.com>
2021-04-26 18:57:26 +0200

    Add TS0042  Gangs Remote Switch
    Add TS0042  Gangs Remote Switch
    
    1 click 2 Clicks, No long click

--- Modules/input.py
+++ Modules/input.py
@@ -3707,7 +3707,7 @@ def Decode8095(self, Devices, MsgData, MsgLQI):
         elif MsgCmd == '01':
             MajDomoDevice(self, Devices, MsgSrcAddr, "01", "0006", '01')
 
-    elif _ModelName in ( "TS0041", "TS0043", "TS0044"): # Tuya remote
+    elif _ModelName in ( "TS0041", "TS0043", "TS0044", "TS0042"): # Tuya remote
         self.log.logging( "Input","Log","Decode8095 - Tuya %s  Addr: %s, Ep: %s, Cluster: %s, Cmd: %s, MsgPayload: %s "
                 % (_ModelName, MsgSrcAddr, MsgEP, MsgClusterId, MsgCmd, MsgPayload), MsgSrcAddr, )
         if MsgCmd[0:2] == 'fd' and MsgPayload:
