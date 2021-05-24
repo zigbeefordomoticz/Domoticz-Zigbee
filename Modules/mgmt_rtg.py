82949ee606bf96a8dd33e3f6310595f2cb8b6ef1
Patrick Pichon <patrick@pichon.me>
2021-05-24 09:17:57 +0200

    cleaning Logs

--- Modules/mgmt_rtg.py
+++ Modules/mgmt_rtg.py
@@ -22,10 +22,9 @@ STATUS_OF_ROUTE = {
 }
 
 def mgmt_rtg(self, nwkid):
-
     if 'RoutingTable' not in self.ListOfDevices[ nwkid ]:
         self.ListOfDevices[ nwkid ]['RoutingTable'] = {}
-        self.ListOfDevices[ nwkid ]['RoutingTable']['Devices'] = {}
+        self.ListOfDevices[ nwkid ]['RoutingTable']['Devices'] = []
         self.ListOfDevices[ nwkid ]['RoutingTable']['SQN'] = 0
         self.ListOfDevices[ nwkid ]['RoutingTable']['TimeStamp'] = time.time()
         mgt_routing_req( self, nwkid, '00')
@@ -39,14 +38,13 @@ def mgmt_rtg(self, nwkid):
     if 'Status' in self.ListOfDevices[ nwkid ]['RoutingTable'] and self.ListOfDevices[ nwkid ]['RoutingTable']['Status'] != STATUS_CODE['00']:
         return
 
-    if (time.time() > self.ListOfDevices[ nwkid ]['RoutingTable']['TimeStamp'] + 3600):
+    feq = self.pluginconf.pluginConf['RoutingTableRequestFeq']
+    if (time.time() > self.ListOfDevices[ nwkid ]['RoutingTable']['TimeStamp'] + feq):
         mgt_routing_req( self, nwkid, '00')
         return
 
 
 def mgmt_rtg_rsp( self, srcnwkid, MsgSourcePoint, MsgClusterID, dstnwkid, MsgDestPoint, MsgPayload, ):
-
-    #Domoticz.Log("mgmt_rtg_rsp --> %s" %MsgPayload)
 
     Sqn = MsgPayload[0:2]
     Status =  MsgPayload[2:4]
@@ -54,10 +52,9 @@ def mgmt_rtg_rsp( self, srcnwkid, MsgSourcePoint, MsgClusterID, dstnwkid, MsgDes
     RoutingTableIndex = MsgPayload[6:8]
     RoutingTableListCount = MsgPayload[8:10]
     RoutingTableListRecord = MsgPayload[10:]
-
     if 'RoutingTable' not in self.ListOfDevices[ srcnwkid ]:
         self.ListOfDevices[ srcnwkid ]['RoutingTable'] = {}
-        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'] = {}
+        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'] = []
         self.ListOfDevices[ srcnwkid ]['RoutingTable']['SQN'] = 0
 
     self.ListOfDevices[ srcnwkid ]['RoutingTable']['TimeStamp'] = time.time()
@@ -67,9 +64,8 @@ def mgmt_rtg_rsp( self, srcnwkid, MsgSourcePoint, MsgClusterID, dstnwkid, MsgDes
     else:
         self.ListOfDevices[ srcnwkid ]['RoutingTable']['Status'] = Status
 
-    Domoticz.Log("Status: %s RoutingTableSize: %s RoutingTableIndex: %s RoutingTableIndex: %s RoutingTableListCount: %s" %(
-        Status, RoutingTableSize, RoutingTableIndex, RoutingTableIndex, RoutingTableListCount ))
-    #Domoticz.Log("RoutingListRecord: %s" %RoutingTableListRecord)
+    Domoticz.Log("mgmt_rtg_rsp %s/%s Status: %s RoutingTableSize: %s RoutingTableIndex: %s RoutingTableIndex: %s RoutingTableListCount: %s" %(
+        srcnwkid, MsgSourcePoint, Status, RoutingTableSize, RoutingTableIndex, RoutingTableIndex, RoutingTableListCount ))
 
     if Status != '00':
         return
@@ -78,6 +74,7 @@ def mgmt_rtg_rsp( self, srcnwkid, MsgSourcePoint, MsgClusterID, dstnwkid, MsgDes
         Domoticz.Log("Incorrect lenght RoutingListRecord: %s" %RoutingTableListRecord)
         return
     while idx < len(RoutingTableListRecord):
+
         target_nwkid = RoutingTableListRecord[ idx+2: idx+4] + RoutingTableListRecord[ idx: idx+2]
         target_bitfields = RoutingTableListRecord[ idx+4: idx+6]
         next_hop =  RoutingTableListRecord[ idx+8: idx+10] + RoutingTableListRecord[ idx+6: idx+8]
@@ -88,27 +85,24 @@ def mgmt_rtg_rsp( self, srcnwkid, MsgSourcePoint, MsgClusterID, dstnwkid, MsgDes
         many_to_one =              ( int(target_bitfields,16) & 0b00010000) >> 4
         route_record_required =    ( int(target_bitfields,16) & 0b00100000) >> 5
 
-        #Domoticz.Log("Target: %s device_status: %s  device_memory_constraint: %s many_to_one: %s route_record_required: %s" %(
-        #    target_nwkid, device_status, device_memory_constraint, many_to_one, route_record_required))
 
-        #if srcnwkid not in self.ListOfDevices:
-        #    return
-
-        if target_nwkid not in self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices']:
-            self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ] = {}
+        routing_record = {}
+        routing_record[ target_nwkid ] = {}
         if device_status in STATUS_OF_ROUTE:
-            self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['Status'] = STATUS_OF_ROUTE[device_status]
+            routing_record[ target_nwkid ]['Status'] = STATUS_OF_ROUTE[device_status]
         else:
-            self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['Status'] = "Unknown (%s)" %device_status
-        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['MemoryConstrained'] = device_memory_constraint
-        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['ManyToOne'] = many_to_one
-        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['RouteRecordRequired'] = route_record_required
-        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'][ target_nwkid ]['NextHopNwkId'] = next_hop
+            routing_record[ target_nwkid ]['Status'] = "Unknown (%s)" %device_status
+        routing_record[ target_nwkid ]['MemoryConstrained'] = device_memory_constraint
+        routing_record[ target_nwkid ]['ManyToOne'] = many_to_one
+        routing_record[ target_nwkid ]['RouteRecordRequired'] = route_record_required
+        routing_record[ target_nwkid ]['NextHopNwkId'] = next_hop
 
-        Domoticz.Log("mgmt_rtg_rsp %s/%s Destination Address: %s Status: %s Memory Constrained: %s Many-to-one: %s Route record required: %s Next-hop address: %s" %(
-            srcnwkid, MsgSourcePoint, target_nwkid, device_status, device_memory_constraint, many_to_one, route_record_required, next_hop ))
+        self.ListOfDevices[ srcnwkid ]['RoutingTable']['Devices'].append (routing_record )
+
+        Domoticz.Log("------------ Destination Address: %s Status: %s Memory Constrained: %s Many-to-one: %s Route record required: %s Next-hop address: %s" %(
+            target_nwkid, device_status, device_memory_constraint, many_to_one, route_record_required, next_hop ))
 
     if int(RoutingTableIndex,16) + int(RoutingTableListCount,16) < int(RoutingTableSize,16):
-        Domoticz.Log("mgmt_rtg_rsp - Next Index for %s RoutingTableIndex: %s RoutingTableListCount: %s RoutingTableSize: %s" %(
+        Domoticz.Log("------------  - Next Index for %s RoutingTableIndex: %s RoutingTableListCount: %s RoutingTableSize: %s" %(
             (srcnwkid, RoutingTableIndex,RoutingTableListCount, RoutingTableSize )))
         mgt_routing_req( self, srcnwkid, '%02x' %(int(RoutingTableIndex,16) + int(RoutingTableListCount,16)) )
