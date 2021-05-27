c471e929ef229de642a3b9d279493c75069c7853
Patrick Pichon <patrick@pichon.me>
2021-05-27 13:37:05 +0200

    correct issue for Change Channel

--- Modules/input.py
+++ Modules/input.py
@@ -658,11 +658,12 @@ def Decode8009(self, Devices, MsgData, MsgLQI):  # Network State response (Firm 
         + str(int(Channel, 16)),
     )
 
-    if ( '0000' in self.ListOfDevices['0000'] 
+
+    if ( '0000' in self.ListOfDevices 
             and 'CheckChannel' in self.ListOfDevices['0000'] 
-            and self.ListOfDevices['0000']['CheckChannel']  != 0 
-            and self.ListOfDevices['0000']['CheckChannel']  == int(Channel,16)):
-        self.ListOfDevices['0000']['CheckChannel']  = 0
+            and self.ListOfDevices['0000']['CheckChannel']  != 0):
+        if self.ListOfDevices['0000']['CheckChannel']  == int(Channel,16):
+            del self.ListOfDevices['0000']['CheckChannel']
 
     if self.ZigateIEEE != extaddr:
         # In order to update the first time
