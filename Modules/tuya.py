bf8951255afb2e0881baa9bada27228193002c3c
Patrick Pichon <patrick@pichon.me>
2021-06-04 18:16:56 +0200

    intercept  Tuya MCU Version RSP

--- Modules/tuya.py
+++ Modules/tuya.py
@@ -161,15 +161,18 @@ def tuyaReadRawAPS(self, Devices, NwkId, srcEp, ClusterID, dstNWKID, dstEP, MsgP
         tuya_response( self,Devices, _ModelName, NwkId, srcEp, ClusterID, dstNWKID, dstEP, dp, datatype, data )
 
     elif cmd == '11': # MCU_VERSION_RSP ( Return version or actively report version )
-        #status = MsgPayload[6:8]   #uint8
-        #transid = MsgPayload[8:10] # uint8
-        #dp = int(MsgPayload[10:12],16)
-        #datatype = int(MsgPayload[12:14],16)
-        #fn = MsgPayload[14:16]
-        #len_data = MsgPayload[16:18]
-        #data = MsgPayload[18:]
-        #store_tuya_attribute( self, NwkId, 'TUYA_MCU_VERSION_RSP', data )
-        pass
+        try:
+            status = MsgPayload[6:8]   #uint8
+            transid = MsgPayload[8:10] # uint8
+            dp = int(MsgPayload[10:12],16)
+            datatype = int(MsgPayload[12:14],16)
+            fn = MsgPayload[14:16]
+            len_data = MsgPayload[16:18]
+            data = MsgPayload[18:]
+            store_tuya_attribute( self, NwkId, 'TUYA_MCU_VERSION_RSP', data )
+        except:
+            Domoticz.Error("tuyaReadRawAPS - MCU_VERSION_RSP error on Payload: %s" %MsgPayload)
+        
 
 
     elif cmd == '23': # TUYA_REPORT_LOG
