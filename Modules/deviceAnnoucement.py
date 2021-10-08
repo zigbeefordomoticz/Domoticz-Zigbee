71e65807befcb22bd88859e134b08240ec652250
Patrick Pichon <patrick@pichon.me>
2021-10-08 07:58:02 +0200

    When device is in Left status, then make sure that fullreprovisionning is done

--- Modules/deviceAnnoucement.py
+++ Modules/deviceAnnoucement.py
@@ -119,7 +119,7 @@ def device_annoucementv2(self, Devices, MsgData, MsgLQI):
 
     reseted_device = False
 
-    if NwkId in self.ListOfDevices and "Status" in self.ListOfDevices[NwkId] and self.ListOfDevices[NwkId]["Status"] in ("erasePDM", "provREQ"):
+    if NwkId in self.ListOfDevices and "Status" in self.ListOfDevices[NwkId] and self.ListOfDevices[NwkId]["Status"] in ("erasePDM", "provREQ", "Left"):
         reseted_device = True
         if "Bind" in self.ListOfDevices[NwkId]:
             del self.ListOfDevices[NwkId]["Bind"]
