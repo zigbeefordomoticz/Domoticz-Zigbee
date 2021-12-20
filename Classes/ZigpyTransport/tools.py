d395ae25b2dc284470094ebd8615f39c3965b0cd
Patrick Pichon <patrick@pichon.me>
2021-12-20 18:45:54 +0100

    makes error message cleaner

--- Classes/ZigpyTransport/tools.py
+++ Classes/ZigpyTransport/tools.py
@@ -1,8 +1,10 @@
 import Domoticz
 
-def handle_thread_error(self, e):
+def handle_thread_error(self, e, data=""):
     trace = []
     tb = e.__traceback__
+    Domoticz.Error("Issue in request, dumping stack")
+    Domoticz.Error("==>  %s" %data)
     Domoticz.Error("'%s' failed '%s'" % (tb.tb_frame.f_code.co_name, str(e)))
     while tb is not None:
         Domoticz.Error(
