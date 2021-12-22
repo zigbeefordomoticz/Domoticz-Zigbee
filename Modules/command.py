9610fd0f2b3d72f5db30f5411d3ed7276b097033
Patrick Pichon <patrick@pichon.me>
2021-12-22 09:30:48 +0100

    Mistake on command sent to open venetian

--- Modules/command.py
+++ Modules/command.py
@@ -568,7 +568,7 @@ def mgtCommand(self, Devices, Unit, Command, Level, Color):
                 actuator_on(self, NWKID, EPout, "Light")
                 #sendZigateCmd(self, "0092", "02" + NWKID + ZIGATE_EP + EPout + "01")
             else:
-                actuator_off(self, NWKID, EPout, "WindowCovering")
+                actuator_on(self, NWKID, EPout, "WindowCovering")
                 #sendZigateCmd(self, "00FA", "02" + NWKID + ZIGATE_EP + EPout + "01")  # Venetian/Blind (On, for Open)
 
         elif DeviceType == "HeatingSwitch":
