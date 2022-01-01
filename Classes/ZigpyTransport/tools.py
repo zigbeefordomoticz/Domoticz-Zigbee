f151f9e51f089e9a4535c429f6b7dc8e65546913
Patrick Pichon <patrick@pichon.me>
2022-01-01 23:06:25 +0100

    Using normal traceback as suggested by @puddly

--- Classes/ZigpyTransport/tools.py
+++ Classes/ZigpyTransport/tools.py
@@ -1,21 +1,28 @@
 import Domoticz
+import traceback
+
+#def handle_thread_error(self, e, data=""):
+#    trace = []
+#    tb = e.__traceback__
+#    self.log.logging("TransportWrter", "Error", "Issue in request, dumping stack")
+#    self.log.logging("TransportWrter", "Error", "==>  %s" % data)
+#    self.log.logging("TransportWrter", "Error", "'%s' failed '%s'" % (tb.tb_frame.f_code.co_name, str(e)))
+#    while tb is not None:
+#        self.log.logging(
+#            "TransportWrter",
+#            "Error",
+#            "----> Line %s in '%s', function %s"
+#            % (
+#                tb.tb_lineno,
+#                tb.tb_frame.f_code.co_filename,
+#                tb.tb_frame.f_code.co_name,
+#            ),
+#        )
+#        tb = tb.tb_next
 
 
 def handle_thread_error(self, e, data=""):
-    trace = []
-    tb = e.__traceback__
+
     self.log.logging("TransportWrter", "Error", "Issue in request, dumping stack")
     self.log.logging("TransportWrter", "Error", "==>  %s" % data)
-    self.log.logging("TransportWrter", "Error", "'%s' failed '%s'" % (tb.tb_frame.f_code.co_name, str(e)))
-    while tb is not None:
-        self.log.logging(
-            "TransportWrter",
-            "Error",
-            "----> Line %s in '%s', function %s"
-            % (
-                tb.tb_lineno,
-                tb.tb_frame.f_code.co_filename,
-                tb.tb_frame.f_code.co_name,
-            ),
-        )
-        tb = tb.tb_next
+    self.log.logging("TransportWrter", "Error", "%s" % (traceback.format_exc() ))
