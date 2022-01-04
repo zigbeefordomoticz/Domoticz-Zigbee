d9a5c7192725cea8307d27e73a0d81cf4dc7218c
SylvainPer <sylperon@gmail.com>
2022-01-04 22:57:18 +0100

    Add log filter on Thread name

--- Classes/PluginConf.py
+++ Classes/PluginConf.py
@@ -1222,14 +1222,6 @@ SETTINGS = {
                 "hidden": False,
                 "Advanced": True,
             },
-            "debugThreadZigpy": {
-                "type": "bool",
-                "default": 0,
-                "current": None,
-                "restart": 0,
-                "hidden": False,
-                "Advanced": True,
-            },
             "debugThreadWriter": {
                 "type": "bool",
                 "default": 0,
@@ -1238,7 +1230,7 @@ SETTINGS = {
                 "hidden": False,
                 "Advanced": True,
             },
-            "debugThreadSerial": {
+            "debugThreadCommunication": {
                 "type": "bool",
                 "default": 0,
                 "current": None,
@@ -1284,12 +1276,13 @@ SETTINGS = {
                 "hidden": False,
                 "Advanced": True,
             },
-                
         },
     },
     "Patching": {
         "Order": 14,
-        "param": {"Bug566": {"type": "bool", "default": 0, "current": None, "restart": 0, "hidden": False, "Advanced": True}},
+        "param": {
+            "Bug566": {"type": "bool", "default": 0, "current": None, "restart": 0, "hidden": False, "Advanced": True}
+        },
     },
     # Experimental
     "Experimental": {
@@ -1871,7 +1864,10 @@ def _load_Settings(self):
         if "TimeStamp" in _domoticz_pluginConf:
             dz_timestamp = _domoticz_pluginConf["TimeStamp"]
             _domoticz_pluginConf = _domoticz_pluginConf["b64Settings"]
-            Domoticz.Log("Plugin data loaded where saved on %s" % (time.strftime("%A, %Y-%m-%d %H:%M:%S", time.localtime(dz_timestamp))))
+            Domoticz.Log(
+                "Plugin data loaded where saved on %s"
+                % (time.strftime("%A, %Y-%m-%d %H:%M:%S", time.localtime(dz_timestamp)))
+            )
         if not isinstance(_domoticz_pluginConf, dict):
             _domoticz_pluginConf = {}
 
@@ -1904,7 +1900,9 @@ def _load_Settings(self):
                 if x not in _domoticz_pluginConf:
                     Domoticz.Error("-- %s is missing in Dz" % x)
                 elif _pluginConf[x] != _domoticz_pluginConf[x]:
-                    Domoticz.Error("++ %s is different in Dz: %s from Json: %s" % (x, _domoticz_pluginConf[x], _pluginConf[x]))
+                    Domoticz.Error(
+                        "++ %s is different in Dz: %s from Json: %s" % (x, _domoticz_pluginConf[x], _pluginConf[x])
+                    )
 
 
 def _load_oldfashon(self, homedir, hardwareid):
@@ -1944,14 +1942,20 @@ def _import_oldfashon_param(self, tmpPluginConf, filename):
                         if is_hex(PluginConf.get(param)):
                             self.pluginConf[param] = int(PluginConf[param], 16)
                         else:
-                            Domoticz.Error("Wrong parameter type for %s, keeping default %s" % (param, self.pluginConf[param]["default"]))
+                            Domoticz.Error(
+                                "Wrong parameter type for %s, keeping default %s"
+                                % (param, self.pluginConf[param]["default"])
+                            )
                             self.pluginConf[param] = self.pluginConf[param]["default"]
 
                     elif SETTINGS[theme]["param"][param]["type"] in ("bool", "int"):
                         if PluginConf.get(param).isdigit():
                             self.pluginConf[param] = int(PluginConf[param])
                         else:
-                            Domoticz.Error("Wrong parameter type for %s, keeping default %s" % (param, self.pluginConf[param]["default"]))
+                            Domoticz.Error(
+                                "Wrong parameter type for %s, keeping default %s"
+                                % (param, self.pluginConf[param]["default"])
+                            )
                             self.pluginConf[param] = self.pluginConf[param]["default"]
                     elif SETTINGS[theme]["param"][param]["type"] == ("path", "str"):
                         self.pluginConf[param] = PluginConf[param]
