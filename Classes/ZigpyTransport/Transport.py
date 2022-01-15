18290ad175e53c514ec707d7e748898ef2f71817
Sylvain PERON <48243214+SylvainPer@users.noreply.github.com>
2022-01-15 19:10:44 +0100

    Update MaxLoad calculation

--- Classes/ZigpyTransport/Transport.py
+++ Classes/ZigpyTransport/Transport.py
@@ -78,15 +78,16 @@ class ZigpyTransport(object):
         self.forwarder_thread.join()
 
     def sendData(self, cmd, datas, sqn=None, highpriority=False, ackIsDisabled=False, waitForResponseIn=False, NwkId=None):
-        if self.writer_queue.qsize() > self.statistics._MaxLoad:
-            self.statistics._MaxLoad = self.writer_queue.qsize()
+        _queue = self.loadTransmit()
+        if _queue > self.statistics._MaxLoad:
+            self.statistics._MaxLoad = _queue
 
         if self.pluginconf.pluginConf["debugzigateCmd"]:
             self.log.logging(
                 "Transport",
                 "Log",
                 "sendData       - [%s] %s %s %s Queue Length: %s"
-                % (sqn, cmd, datas, NwkId, self.writer_queue.qsize()),
+                % (sqn, cmd, datas, NwkId, _queue),
             )
 
         self.log.logging("Transport", "Debug", "===> sendData - Cmd: %s Datas: %s" % (cmd, datas))
