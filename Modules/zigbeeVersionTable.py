3e84db79b158836d6532ca82cd19cb6ee17fc669
@pipiche38 <patrick@pichon.me>
2022-03-08 13:19:13 +0000

    include Elelabs

--- Modules/zigbeeVersionTable.py
+++ Modules/zigbeeVersionTable.py
@@ -5,6 +5,7 @@ FIRMWARE_BRANCH = {
     "20": "CC1352/CC2652, Z-Stack 3.30+ (znp)",
     "21": "CC2531, Z-Stack 3.0.x (znp)",
     "22": "CC2531, Z-Stack Home 1.2 (znp",
+    "30": "Elelabs, ELR023",
     
     "99": "Unknown"
 }
@@ -13,4 +14,5 @@ ZNP_MODEL = {
     "CC1352/CC2652, Z-Stack 3.30+": "20",
     "CC2531, Z-Stack 3.0.x":        "21",
     "CC2531, Z-Stack Home 1.2":     "22",
+    "Elelabs, ELR023":              "30",
 }
