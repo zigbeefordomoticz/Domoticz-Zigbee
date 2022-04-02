9f032d19762ff60a183ecbac63a92167b02e8812
Pipiche <pipiche38@users.noreply.github.com>
2022-04-02 15:15:48 +0200

    Pipiche38/zigpy/fetchupstream02042022 (#1124)
    * merge zigpy and bellows upstream update
    * Plugin beta6 version 6.2.002

--- plugin.py
+++ plugin.py
@@ -1542,7 +1542,7 @@ def check_python_modules_version( self ):
     MODULES_VERSION = {
         "dns": "2.2.0rc1",
         "serial": "3.5",
-        "zigpy": "0.44.0.dev0",
+        "zigpy": "0.45.0.dev0",
         "zigpy_znp": "0.7.0",
         "zigpy_deconz": "0.15.0.dev0",
         "zigpy_zigate": "0.8.0",
