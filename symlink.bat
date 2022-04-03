519737c52cc36564bd32d15d72c2f3b495babfb8
Patrick Pichon <patrick@pichon.me>
2022-04-03 19:50:35 +0200

    update symlink

--- symlink.bat
+++ symlink.bat
@@ -1,3 +1,4 @@
+REM This tool allows the creation of the Windows Symbolic links after a git clone
 
 del dns bellows serial zigpy zigpy_zigate zigpy_znp zigpy_znp
 
@@ -7,4 +8,4 @@ mklink /D serial external\pyserial\serial
 mklink /D zigpy external\zigpy\zigpy
 mklink /D zigpy_deconz external\zigpy-deconz\zigpy_deconz
 mklink /D zigpy_zigate external\zigpy-zigate\zigpy_zigate
-mklink /D zigpy\znp external\zigpy-znp\zigpy_znp
+mklink /D zigpy_znp external\zigpy-znp\zigpy_znp
