a201d54e3d6231fc2c84938b0d59076c34e31098
Pichon <patrick@hpe.com>
2022-04-03 19:40:31 +0200

    create script to create the symb links

---
+++ symlink.bat
@@ -0,0 +1,10 @@
+
+del dns bellows serial zigpy zigpy_zigate zigpy_znp zigpy_znp
+
+mklink /D dns external\dnspython\dns
+mklink /D bellows external\bellows\bellow
+mklink /D serial external\pyserial\serial
+mklink /D zigpy external\zigpy\zigpy
+mklink /D zigpy_deconz external\zigpy-deconz\zigpy_deconz
+mklink /D zigpy_zigate external\zigpy-zigate\zigpy_zigate
+mklink /D zigpy\znp external\zigpy-znp\zigpy_znp
