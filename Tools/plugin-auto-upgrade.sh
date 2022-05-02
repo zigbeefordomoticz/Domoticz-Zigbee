598693e64a4554d21b011792ec896aa45dec9cf0
Patrick Pichon <patrick@pichon.me>
2022-05-02 19:57:59 +0200

    first version of the Tools/plugin-auto-upgrade.sh

--- Tools/plugin-auto-upgrade.sh
+++ Tools/plugin-auto-upgrade.sh
@@ -2,8 +2,38 @@
 
 
 
-echo "Starting Plugin Upgrade process."
+echo "Starting Zigbee for Domoticz plugin Upgrade process."
+echo "----------------------------------------------------"
+
+echo "Checking pip3 and upgrading if needed"
+sudo python3 -m pip install --upgrade pip
+ret="$?"
+if [ "$ret" != "0" ] ; then
+    echo "Problem while running command 'sudo python3 -m pip install --upgrade pip.'"
+    echo "Is sudo available for this user without password ?"
+    echo "\n"
+    exit -1
+fi
+
+echo "(1) update python3 modules if needed"
+sudo python3 -m pip --no-input install -r requirements.txt
+ret="$?"
+if [ "$ret" != "0" ] ; then
+    echo "Problem while running command 'sudo python3 -m pip --no-input install -r requirements.txt'."
+    echo "Is sudo available for this user without password ?"
+    exit -2
+fi
+
+
+echo "(2) updating Zigbee for Domoticz plugin"
+git config --add submodule.recurse true
+git pull --recurse-submodules
+ret="$?""
+if [ "$ret" != "0" ] ; then
+    echo "Problem while running command 'git pull --recurse-submodules'."
+    exit -2
+fi
+
 
 echo "Plugin Upgrade process completed without errors."
-
 exit 0
