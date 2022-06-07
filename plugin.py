78d35018500fc9ed58eb1b2cc7d6eebedce4c724
Patrick Pichon <patrick@pichon.me>
2022-06-07 11:21:09 +0200

    more precise on default for API url base

--- plugin.py
+++ plugin.py
@@ -46,7 +46,7 @@
         </param>
         <param field="Mode5" label="API base url <br/>(http://username:password@127.0.0.1:port)" width="250px" default="http://127.0.0.1:/8080" required="true" >
             <description>
-                <br/><h3>Domoticz Json/API base</h3>In case Domoticz listen to an other port change 8080 by what ever is the port, 
+                <br/><h3>Domoticz Json/API base ( http://127.0.0.1:8080 should be the default)</h3>In case Domoticz listen to an other port change 8080 by what ever is the port, 
                 and if you have setup an authentication please add the username:password</description>
         </param>
         <param field="Mode4" label="WebUI port" width="75px" required="true" default="9440" >
