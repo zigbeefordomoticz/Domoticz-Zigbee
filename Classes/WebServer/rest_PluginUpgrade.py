1b684b544162e4f5f31a4102e3fbf756546ab76a
Patrick Pichon <patrick@pichon.me>
2022-07-13 15:46:26 +0200

    log results of the plugin upgrade in the Plugin log

--- Classes/WebServer/rest_PluginUpgrade.py
+++ Classes/WebServer/rest_PluginUpgrade.py
@@ -16,16 +16,15 @@ def rest_plugin_upgrade(self, verb, data, parameters):
     result = {}
     _response = prepResponseMessage(self, setupHeadersResponse())
     if verb == "GET" and len(parameters) == 0:
-        
+        self.logging("Log", "Plugin Upgrade starting: %s" %(upgrade_script))
         process = subprocess.run( upgrade_script ,
                                  cwd= self.pluginParameters["HomeFolder"],
                                 universal_newlines=True,
                                 text=True,
                                 capture_output=True,
                                 shell=True)
-        
         result = {"result": str(process.stdout), "ReturnCode": process.returncode }
-        
     _response["Data"] = json.dumps(result, sort_keys=True)
+    self.logging("Log", "Plugin Upgrade results: %s" %(result))
 
     return _response
