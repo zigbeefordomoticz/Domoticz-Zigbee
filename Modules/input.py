734c633402ac11ce851d34c73f6b8e6ec0edf9ce
Patrick PICHON <patrick@pichon.me>
2022-08-21 12:49:18 +0200

    more debug

--- Modules/input.py
+++ Modules/input.py
@@ -3024,6 +3024,16 @@ def Decode8102(self, Devices, MsgData, MsgLQI):  # Attribute Reports
 
 def scan_attribute_reponse(self, Devices, MsgSQN, i_sqn, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgData, msgtype):
 
+    self.log.logging(
+        "Input",
+        "Debug",
+        "scan_attribute_reponse - Sqn: %s i_sqn: %s Nwkid: %s Ep: %s Cluster: %s MsgData: %s Type: %s"
+        % (
+            MsgSQN, i_sqn, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgData, msgtype
+        ),
+        MsgSrcAddr,
+    )
+
     idx = 12
     while idx < len(MsgData):
         MsgAttrID = MsgAttStatus = MsgAttType = MsgAttSize = MsgClusterData = ""
@@ -3037,8 +3047,9 @@ def scan_attribute_reponse(self, Devices, MsgSQN, i_sqn, MsgSrcAddr, MsgSrcEp, M
             MsgAttSize = MsgData[idx : idx + 4]
             idx += 4
             size = int(MsgAttSize, 16) * 2
-            MsgClusterData = MsgData[idx : idx + size]
-            idx += size
+            if size > 0:
+                MsgClusterData = MsgData[idx : idx + size]
+                idx += size
         else:
             self.log.logging(
                 "Input",
