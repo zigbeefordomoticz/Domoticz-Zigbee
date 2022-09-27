bde7fd1bc1f0a4cec7875bbf4760717cb463b43b
Patrick Pichon <patrick@pichon.me>
2022-09-27 21:22:24 +0200

    fix decoding of fc00 frame when coming from zigpy. Add a RWL021_Dimmer_step parameter to define the progressing step of the Level

--- Modules/philips.py
+++ Modules/philips.py
@@ -3,17 +3,20 @@
 #
 # Author: zaraki673 & pipiche38
 #
-import Domoticz
 import struct
 
+import Domoticz
+
+import Modules.paramDevice
 from Modules.basicOutputs import (raw_APS_request, set_poweron_afteroffon,
                                   write_attribute)
+from Modules.domoMaj import MajDomoDevice
 from Modules.readAttributes import (ReadAttributeRequest_0006_0000,
                                     ReadAttributeRequest_0006_400x,
                                     ReadAttributeRequest_0008_0000,
                                     ReadAttributeRequest_0406_philips_0030)
-from Modules.domoMaj import MajDomoDevice
-from Modules.tools import is_hex, retreive_cmd_payload_from_8002, checkAndStoreAttributeValue
+from Modules.tools import (checkAndStoreAttributeValue, is_hex,
+                           retreive_cmd_payload_from_8002)
 from Modules.zigateConsts import ZIGATE_EP
 
 PHILIPS_POWERON_MODE = {0x00: "Off", 0x01: "On", 0xFF: "Previous state"}  # Off  # On  # Previous state
@@ -92,12 +95,11 @@ def philipsReadRawAPS(self, Devices, srcNWKID, srcEp, ClusterID, dstNWKID, dstEP
     if self.zigbee_communication == "native" and _ModelName == "RWL021" and cmd == "00" and ClusterID == "fc00":
         # This is handle by the firmware
         return
-    elif  _ModelName == "RWL021" and cmd == "00" and ClusterID == "fc00":
-         philips_dimmer_switch( self, Devices, srcNWKID, srcEp, ClusterID, dstNWKID, dstEP, MsgPayload)
-
+    elif self.zigbee_communication == "zigpy" and _ModelName == "RWL021" and cmd == "00" and ClusterID == "fc00":
+        zigpy_philips_dimmer_switch(self, Devices, srcNWKID, srcEp, ClusterID, dstNWKID, dstEP, MsgPayload)
     self.log.logging(
         "Philips",
-        "Log",
+        "Debug",
         "philipsReadRawAPS - Nwkid: %s/%s Cluster: %s, GlobalCommand: %s sqn: %s, Command: %s Payload: %s"
         % (srcNWKID, srcEp, ClusterID, GlobalCommand, sqn, cmd, data),
     )
@@ -153,167 +155,119 @@ def philips_set_poweron_after_offon_device(self, mode, nwkid):
     set_poweron_afteroffon(self, nwkid, OnOffMode=mode)
     ReadAttributeRequest_0006_400x(self, nwkid)
 
+def zigpy_philips_dimmer_switch(self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, dstNWKID, dstEP, MsgPayload):
+    #        1d/0b10/2400/0300/00/30|01|21|1800 (long press)
+    #                                      xxxx -> duration
+    #                                xx-> Action
+    #                     xxxx -> Attribute Id
+    #           xxxx -> Manuf code
+   
+    MsgAttrID = "%04x" %struct.unpack("H", struct.pack(">H", int( MsgPayload[10:14], 16)))[0] 
+    MsgClusterData = MsgPayload[14+2:]
+    philips_dimmer_switch(self,  Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, MsgClusterData)
 
+def philips_dimmer_switch(self,  Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, MsgClusterData):
 
-def philips_dimmer_switch(self,  Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, dstNWKID, dstEP, MsgPayload):
+    self.log.logging( "Philips", "Debug", "philips_dimmer_switch %s - %s/%s - MsgAttrID = %s MsgClusterData: %s" % ( 
+        MsgSrcAddr, MsgSrcEp, MsgClusterId,MsgAttrID, MsgClusterData ))
 
-    # On:  1d/0b10/23/0001/00003/000210000
-    # Off: 1d/0b10/26/0004/00003/002210100
-    # Plus:1d/0b10/28/0002/00003/002210200
-    
-    MsgAttrID = MsgPayload[8:12]
-    MsgClusterData = MsgPayload[12:]
-    
-    self.log.logging(
-        "Cluster",
-        "Debug",
-        "ReadCluster %s - %s/%s - reading self.ListOfDevices[%s]['Ep'][%s][%s][%s] = %s"
-        % (
-            MsgClusterId,
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgClusterId,
-            MsgAttrID,
-            self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId],
-        ),
-        MsgSrcAddr,
-    )
+    self.log.logging( "Philips", "Debug", "philips_dimmer_switch %s - %s/%s - reading self.ListOfDevices[%s]['Ep'][%s][%s][%s] = %s" % ( 
+        MsgClusterId, MsgSrcAddr, MsgSrcEp, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId], ), MsgSrcAddr, )
 
-    DIMMER_STEP = 1
-    if "0000" in self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId]:
-        prev_Value = str(self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId]["0000"]).split(";")
-        if len(prev_Value) == 3:
-            for val in prev_Value:
-                if not is_hex(val):
-                    prev_Value = "0;80;0".split(";")
-                    break
-        else:
-            prev_Value = "0;80;0".split(";")
-    else:
-        prev_Value = "0;80;0".split(";")
+    rwl021_dimmer_step = Modules.paramDevice.get_device_config_param(self, MsgSrcAddr, "RWL021_Dimmer_step")
+    if rwl021_dimmer_step is None:
+        rwl021_dimmer_step = 1
+        
+    self.log.logging( "Philips", "Debug", "philips_dimmer_switch %s/%s - dimming step: %s" %( MsgSrcAddr, MsgSrcEp, rwl021_dimmer_step))
+
+    prev_Value = _retreive_current_position( self, MsgSrcAddr, MsgSrcEp, MsgClusterId)
 
     prev_onoffvalue = onoffValue = int(prev_Value[0], 16)
     prev_lvlValue = lvlValue = int(prev_Value[1], 16)
     prev_duration = duration = int(prev_Value[2], 16)
 
-    self.log.logging(
-        "Cluster",
-        "Debug",
-        "ReadCluster - %s - %s/%s - past OnOff: %s, Lvl: %s" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, onoffValue, lvlValue),
-        MsgSrcAddr,
-    )
+    self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - past OnOff: %s, Lvl: %s" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, onoffValue, lvlValue), MsgSrcAddr, )
+
     if MsgAttrID == "0001":  # On button
-        self.log.logging(
-            "Cluster",
-            "Debug",
-            "ReadCluster - %s - %s/%s - ON Button detected" % (MsgClusterId, MsgSrcAddr, MsgSrcEp),
-            MsgSrcAddr,
-        )
-        onoffValue = 1
+        self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - ON Button detected" % (MsgClusterId, MsgSrcAddr, MsgSrcEp), MsgSrcAddr, )
+        return _update_domoticz_widget( self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, prev_onoffvalue, 1, prev_lvlValue, lvlValue, duration )
 
     elif MsgAttrID == "0004":  # Off  Button
-        self.log.logging(
-            "Cluster",
-            "Debug",
-            "ReadCluster - %s - %s/%s - OFF Button detected" % (MsgClusterId, MsgSrcAddr, MsgSrcEp),
-            MsgSrcAddr,
-        )
+        self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - OFF Button detected" % (
+            MsgClusterId, MsgSrcAddr, MsgSrcEp), MsgSrcAddr, )
         onoffValue = 0
-
+        return _update_domoticz_widget( self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, prev_onoffvalue, 0, prev_lvlValue, lvlValue, duration )
+    
     elif MsgAttrID in ("0002", "0003"):  # Dim+ / 0002 is +, 0003 is -
-        self.log.logging(
-            "Cluster",
-            "Debug",
-            "ReadCluster - %s - %s/%s - DIM Button detected" % (MsgClusterId, MsgSrcAddr, MsgSrcEp),
-            MsgSrcAddr,
-        )
+        self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - DIM Button detected" % (
+            MsgClusterId, MsgSrcAddr, MsgSrcEp), MsgSrcAddr, )
         action = MsgClusterData[2:4]
-        duration = MsgClusterData[6:10]
-        duration = struct.unpack("H", struct.pack(">H", int(duration, 16)))[0]
+        duration = struct.unpack("H", struct.pack(">H", int( MsgClusterData[6:10], 16)))[0]
 
-        if action in ("00"):  # Short press
-            self.log.logging(
-                "Cluster",
-                "Debug",
-                "ReadCluster - %s - %s/%s - DIM Action: %s" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, action),
-                MsgSrcAddr,
-            )
+        if action in ("00", ):  # Short press
+            self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - DIM Action: %s" % (
+                MsgClusterId, MsgSrcAddr, MsgSrcEp, action), MsgSrcAddr, )
             onoffValue = 1
             # Short press/Release - Make one step   , we just report the press
             if MsgAttrID == "0002":
-                lvlValue += DIMMER_STEP
+                lvlValue += rwl021_dimmer_step
             elif MsgAttrID == "0003":
-                lvlValue -= DIMMER_STEP
+                lvlValue -= rwl021_dimmer_step
 
-        elif action in ("01"):  # Long press
+        elif action in ("01",):  # Long press
             delta = duration - prev_duration  # Time press since last message
             onoffValue = 1
             if MsgAttrID == "0002":
-                lvlValue += round(delta * DIMMER_STEP)
+                lvlValue += round(delta * rwl021_dimmer_step)
             elif MsgAttrID == "0003":
-                lvlValue -= round(delta * DIMMER_STEP)
+                lvlValue -= round(delta * rwl021_dimmer_step)
 
-        elif action in ("03"):  # Release after Long Press
-            self.log.logging(
-                "Cluster",
-                "Debug",
-                "ReadCluster - %s - %s/%s - DIM Release after %s seconds" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, round(duration / 10)),
-                MsgSrcAddr,
-            )
+        elif action in ("03", "02"):  # Release after Long/short Press
+            self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - DIM Release after %s seconds" % (
+                MsgClusterId, MsgSrcAddr, MsgSrcEp, round(duration / 10)), MsgSrcAddr, )
 
         else:
-            self.log.logging(
-                "Cluster",
-                "Debug",
-                "ReadCluster - %s - %s/%s - DIM Action: %s not processed" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, action),
-                MsgSrcAddr,
-            )
+            self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - DIM Action: %s not processed" % (
+                MsgClusterId, MsgSrcAddr, MsgSrcEp, action), MsgSrcAddr, )
             return  # No need to update
 
         # Check if we reach the limits Min and Max
-        if lvlValue > 255:
-            lvlValue = 255
-        if lvlValue <= 0:
-            lvlValue = 0
-        self.log.logging(
-            "Cluster",
-            "Debug",
-            "ReadCluster - %s - %s/%s - Level: %s " % (MsgClusterId, MsgSrcAddr, MsgSrcEp, lvlValue),
-            MsgSrcAddr,
-        )
+        lvlValue = min(lvlValue, 255)
+        lvlValue = max(lvlValue, 0)
+        self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s - Level: %s " % (
+            MsgClusterId, MsgSrcAddr, MsgSrcEp, lvlValue), MsgSrcAddr, )
+        return _update_domoticz_widget( self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, prev_onoffvalue, onoffValue, prev_lvlValue, lvlValue, duration )
+        
     else:
-        self.log.logging(
-            "Cluster",
-            "Log",
-            "readCluster - %s - %s/%s unknown attribute: %s %s" % (MsgClusterId, MsgSrcAddr, MsgSrcEp, MsgAttrID, MsgClusterData),
-            MsgSrcAddr,
-        )
+        self.log.logging( "Philips", "Debug", "philips_dimmer_switch - %s - %s/%s unknown attribute: %s %s" % (
+            MsgClusterId, MsgSrcAddr, MsgSrcEp, MsgAttrID, MsgClusterData), MsgSrcAddr, )
+    
 
+
+def _update_domoticz_widget( self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, prev_onoffvalue, onoffValue, prev_lvlValue, lvlValue, duration ):
     # Update Domo
     sonoffValue = "%02x" % onoffValue
     slvlValue = "%02x" % lvlValue
     sduration = "%02x" % duration
 
     checkAndStoreAttributeValue(self, MsgSrcAddr, MsgSrcEp, MsgClusterId, "0000", "%s;%s;%s" % (sonoffValue, slvlValue, sduration))
-    self.log.logging(
-        "Cluster",
-        "Debug",
-        "ReadCluster %s - %s/%s - updating self.ListOfDevices[%s]['Ep'][%s][%s] = %s"
-        % (
-            MsgClusterId,
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgClusterId,
-            self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId],
-        ),
-        MsgSrcAddr,
-    )
+    self.log.logging( "Philips", "Debug", "philips_dimmer_switch %s - %s/%s - updating self.ListOfDevices[%s]['Ep'][%s][%s] = %s" % ( 
+        MsgClusterId, MsgSrcAddr, MsgSrcEp, MsgSrcAddr, MsgSrcEp, MsgClusterId, self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId], ),MsgSrcAddr,)
 
     if prev_onoffvalue != onoffValue:
         MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, "0006", sonoffValue)
     if prev_lvlValue != lvlValue:
         MajDomoDevice(self, Devices, MsgSrcAddr, MsgSrcEp, MsgClusterId, slvlValue)
+
+    
+def _retreive_current_position( self, MsgSrcAddr, MsgSrcEp, MsgClusterId):
+    if "0000" not in self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId]:
+        return "0;80;0".split(";")
+    prev_Value = str(self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp][MsgClusterId]["0000"]).split(";")
+    if len(prev_Value) != 3:
+        return "0;80;0".split(";")  
+    for val in prev_Value:
+        if not is_hex(val):
+            prev_Value = "0;80;0".split(";")
+            break
+    return prev_Value
