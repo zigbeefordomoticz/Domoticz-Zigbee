432b7c977ea9ce52337e7e7afa766db48d59305d
Patrick Pichon <patrick@pichon.me>
2022-11-15 18:32:47 +0100

    disable SourceRouting and disable ForceAPSAck

--- Classes/PluginConf.py
+++ Classes/PluginConf.py
@@ -66,9 +66,9 @@ SETTINGS = {
             "autoRestore": {"type": "bool", "default": 1, "current": None, "restart": 0, "hidden": False, "Advanced": True,},
                 
             "BackupFullDevices": { "type": "bool", "default": 0, "current": None, "restart": 0, "hidden": False, "Advanced": False,"ZigpyRadio": "znp" },
-            "ForceAPSAck": { "type": "bool", "default": 1, "current": None, "restart": 0, "hidden": False, "Advanced": True, },
+            "ForceAPSAck": { "type": "bool", "default": 0, "current": None, "restart": 0, "hidden": False, "Advanced": True, },
             "BellowsNoMoreEndDeviceChildren": { "type": "bool", "default": 0, "current": None, "restart": 1, "hidden": False, "Advanced": True, "ZigpyRadio": "ezsp" },
-            "zigpySourceRouting": { "type": "bool", "default": 0, "current": None, "restart": 1, "hidden": False, "Advanced": True, },
+            "zigpySourceRouting": { "type": "bool", "default": 0, "current": None, "restart": 1, "hidden": True, "Advanced": True, },
             "forceZigpy_noasyncio": { "type": "bool", "default": 0, "current": None, "restart": 0, "hidden": True, "Advanced": True, },
         }
     },
