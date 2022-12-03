ca531719b193541b9a3928fdfcd1910ced6bfb8d
Patrick Pichon <patrick@pichon.me>
2022-12-03 19:14:45 +0100

    in case of zigpy, FirmwareVersion can be a non-int

--- Modules/input.py
+++ Modules/input.py
@@ -3226,7 +3226,7 @@ def Decode8110(self, Devices, MsgData, MsgLQI):
     if len(MsgData) != 24:
         MsgAttrStatus = MsgData[12:14]
         MsgAttrID = None
-    elif int(self.FirmwareVersion, 16) < int("31d", 16):
+    elif self.zigbee_communication == "native" and int(self.FirmwareVersion, 16) < int("31d", 16):
         MsgAttrID = MsgData[12:16]
         MsgAttrStatus = MsgData[16:18]
     else:
@@ -3235,72 +3235,26 @@ def Decode8110(self, Devices, MsgData, MsgLQI):
         MsgAttrStatus = MsgData[14:16]
         MsgAttrID = None
 
-    Decode8110_raw(
-        self,
-        Devices,
-        MsgSQN,
-        MsgSrcAddr,
-        MsgSrcEp,
-        MsgClusterId,
-        MsgAttrStatus,
-        MsgAttrID,
-        MsgLQI,
-    )
+    Decode8110_raw( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrStatus, MsgAttrID, MsgLQI, )
 
 
-def Decode8110_raw(
-    self,
-    Devices,
-    MsgSQN,
-    MsgSrcAddr,
-    MsgSrcEp,
-    MsgClusterId,
-    MsgAttrStatus,
-    MsgAttrID,
-    MsgLQI,
-):  # Write Attribute response
+def Decode8110_raw( self, Devices, MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrStatus, MsgAttrID, MsgLQI, ):  # Write Attribute response
 
     i_sqn = sqn_get_internal_sqn_from_app_sqn(self.ControllerLink, MsgSQN, TYPE_APP_ZCL)
-    self.log.logging(
-        "Input",
-        "Debug",
-        "Decode8110 - WriteAttributeResponse - MsgSQN: %s,  MsgSrcAddr: %s, MsgSrcEp: %s, MsgClusterId: %s MsgAttrID: %s Status: %s"
-        % (MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, MsgAttrStatus),
-        MsgSrcAddr,
-    )
+    self.log.logging( "Input", "Debug", "Decode8110 - WriteAttributeResponse - MsgSQN: %s,  MsgSrcAddr: %s, MsgSrcEp: %s, MsgClusterId: %s MsgAttrID: %s Status: %s" % (
+        MsgSQN, MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, MsgAttrStatus), MsgSrcAddr, )
 
     timeStamped(self, MsgSrcAddr, 0x8110)
     updSQN(self, MsgSrcAddr, MsgSQN)
     updLQI(self, MsgSrcAddr, MsgLQI)
     lastSeenUpdate(self, Devices, NwkId=MsgSrcAddr)
 
-    if self.FirmwareVersion and int(self.FirmwareVersion, 16) >= int("31d", 16) and MsgAttrID:
-        set_status_datastruct(
-            self,
-            "WriteAttributes",
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgClusterId,
-            MsgAttrID,
-            MsgAttrStatus,
-        )
-        set_request_phase_datastruct(
-            self,
-            "WriteAttributes",
-            MsgSrcAddr,
-            MsgSrcEp,
-            MsgClusterId,
-            MsgAttrID,
-            "fullfilled",
-        )
+    if (self.zigbee_communication != "native" or (self.FirmwareVersion and int(self.FirmwareVersion, 16) >= int("31d", 16))) and MsgAttrID:
+        set_status_datastruct( self, "WriteAttributes", MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, MsgAttrStatus, )
+        set_request_phase_datastruct( self, "WriteAttributes", MsgSrcAddr, MsgSrcEp, MsgClusterId, MsgAttrID, "fullfilled", )
         if MsgAttrStatus != "00":
-            self.log.logging(
-                "Input",
-                "Log",
-                "Decode8110 - Write Attribute Respons response - ClusterID: %s/%s, MsgSrcAddr: %s, MsgSrcEp:%s , Status: %s"
-                % (MsgClusterId, MsgAttrID, MsgSrcAddr, MsgSrcEp, MsgAttrStatus),
-                MsgSrcAddr,
-            )
+            self.log.logging( "Input", "Log", "Decode8110 - Write Attribute Respons response - ClusterID: %s/%s, MsgSrcAddr: %s, MsgSrcEp:%s , Status: %s" % (
+                MsgClusterId, MsgAttrID, MsgSrcAddr, MsgSrcEp, MsgAttrStatus), MsgSrcAddr, )
         return
 
     # We got a global status for all attributes requested in this command
