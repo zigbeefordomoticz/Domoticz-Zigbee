f831eb901a46d482c9f25c3f0c0bef219db211c9
Patrick Pichon <patrick@pichon.me>
2023-02-14 20:15:55 +0100

    fix and update

--- Modules/tuyaTS0601.py
+++ Modules/tuyaTS0601.py
@@ -2,6 +2,7 @@
 from Modules.domoMaj import MajDomoDevice
 from Modules.tuyaTools import store_tuya_attribute
 from Modules.tools import checkAndStoreAttributeValue
+from Modules.domoTools import Update_Battery_Device
 
 
 # Generic functions
@@ -23,16 +24,19 @@ def ts0601_response(self, Devices, model_name, NwkId, Ep, dp, datatype, data):
     if "EvalExp" in dps_mapping[ str_dp ]:
         value = evaluate_expression_with_data(self, dps_mapping[ str_dp ][ "EvalExp"], value)
 
+    if "store_tuya_attribute" in dps_mapping[ str_dp ]:
+        store_tuya_attribute(self, NwkId, dps_mapping[ str_dp ]["store_tuya_attribute"], data)
+    
     if "sensor_type" not in dps_mapping[ str_dp ]:
-        self.log.logging("Tuya", "Error", "ts0601_response - no sensor_type provided %s %s %s %s" % (
-            NwkId, dp, datatype, data), NwkId)
-        return False
+        if "store_tuya_attribute" not in dps_mapping[ str_dp ]:
+            store_tuya_attribute(self, NwkId, "UnknowDp_0x%02x_Dt_0x%02x" % (dp, datatype) , data)
+        return True
     
     sensor_type = dps_mapping[ str_dp ][ "sensor_type"]
     if sensor_type not in DP_SENSOR_FUNCTION:
         self.log.logging("Tuya", "Error", "ts0601_response - no sensor function found for %s %s %s %s %s" % (
             NwkId, sensor_type, dp, datatype, data), NwkId)
-        return False
+        return True
     
     value = check_domo_format_req( self, dps_mapping[ str_dp ], value)
     
@@ -108,6 +112,15 @@ def ts0601_distance(self, Devices, nwkid, ep, value):
     self.log.logging("Tuya", "Log", "ts0601_distance - Distance %s %s %s " % (nwkid, ep, value), nwkid)
     store_tuya_attribute(self, nwkid, "Distance", value)
     MajDomoDevice(self, Devices, nwkid, ep, "Distance", value)
+
+def ts0601_battery(self, Devices, nwkid, ep, value ):
+    self.log.logging("Tuya", "Log", "ts0601_battery - Battery %s %s %s" % (nwkid, ep, value), nwkid)
+        
+    store_tuya_attribute(self, nwkid, "Battery", value)
+    checkAndStoreAttributeValue(self, nwkid, "01", "0001", "0000", value)
+    self.ListOfDevices[nwkid]["Battery"] = value
+    Update_Battery_Device(self, Devices, nwkid, value)
+    store_tuya_attribute(self, nwkid, "BatteryStatus", value)
 
 
 DP_SENSOR_FUNCTION = {
