941e47cb84b68e501ded3d9a294f4303a1ab2298
Patrick Pichon <patrick@pichon.me>
2023-03-11 12:19:18 +0100

    missing coma on TUYA TEMP+HUMI

--- Modules/tuyaConst.py
+++ Modules/tuyaConst.py
@@ -41,10 +41,10 @@ TUYA_MOTION = (
 )
 
 TUYA_TEMP_HUMI = ( 
-    "_TZE200_qyflbnbj"
+    "_TZE200_qyflbnbj",
     "_TZE200_bjawzodf", 
     "_TZE200_bq5c8xfe", 
-    "_TZE200_qoy0ekbd" ,
+    "_TZE200_qoy0ekbd",
     "_TZE200_whkgqxse"
 )
 
