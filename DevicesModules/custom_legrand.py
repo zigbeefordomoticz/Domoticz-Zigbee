a23f7e8ddfa02188b76d2e80797c7f3bb9b7d34d
Patrick Pichon <patrick@pichon.me>
2023-03-27 12:52:27 +0200

    Compute Legrand Operating time based on strftime() and utcfromtimestamp()

--- DevicesModules/custom_legrand.py
+++ DevicesModules/custom_legrand.py
@@ -1,15 +1,9 @@
 
-
+from datetime import datetime as dt
 
 def legrand_operating_time(self, nwkid, ep, cluster, attribut, value):
-    
-    op_time = value
-    dd = op_time // 62400
-    op_time -= dd * 62400
-    hh = op_time // 3600
-    op_time -= hh * 3600
-    mm = op_time // 60
-    op_time -= mm * 60
-    ss = op_time
-
-    return "%sd %sh %sm %ss" % (dd, hh, mm, ss)
+    try:
+        operating_time = dt.strftime(dt.utcfromtimestamp(value), '%Hh %Mm %Ss')
+    except OverflowError:
+        operating_time = "99h 99m 99s"
+    return operating_time
