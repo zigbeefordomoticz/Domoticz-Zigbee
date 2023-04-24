b5090b39869fe72d83a9ec5be142675366f946e5
Patrick Pichon <patrick@pichon.me>
2023-04-24 19:57:11 +0200

    Fix issues in extract_nwk_infos_from_8002 - TargetAddrMode and TargetNwkId were mixed up. - In case of no payload ( payload is empty ), do not try to decode the frame, and log a message

--- Zigbee/decode8002.py
+++ Zigbee/decode8002.py
@@ -24,6 +24,11 @@ def decode8002_and_process(self, frame):
     if SrcNwkId is None:
         return frame
 
+    if len(Payload) == 0:
+        self.log.logging("Transport8002", "Log", "decode8002_and_process - Frame with empty Payload !! ProfileID: %04x NwkId: %s Ep: %s Cluster: %s frame: %s" % (
+            int(ProfileId,16), SrcNwkId, SrcEndPoint, ClusterId, frame))
+        return frame
+    
     if ProfileId == "0000":
         frame = zdp_decoders(self, SrcNwkId, SrcEndPoint, TargetEp, ClusterId, Payload, frame)
         self.log.logging("Transport8002", "Debug", "decode8002_and_process return ZDP frame: %s" % frame)
@@ -44,10 +49,6 @@ def decode8002_and_process(self, frame):
 
 def extract_nwk_infos_from_8002(frame):
 
-    MsgType = frame[2:6]
-    MsgLength = frame[6:10]
-    MsgCRC = frame[10:12]
-
     if len(frame) < 18:
         return (None, None, None, None, None, None)
 
@@ -63,39 +64,34 @@ def extract_nwk_infos_from_8002(frame):
 
     if int(SrcAddrMode, 16) in [ADDRESS_MODE["short"], ADDRESS_MODE["group"]]:
         SrcNwkId = MsgData[16:20]  # uint16_t
-        TargetNwkId = MsgData[20:22]
+        TargetAddrMode = MsgData[20:22]
 
-        if int(TargetNwkId, 16) in [
-            ADDRESS_MODE["short"],
-            ADDRESS_MODE["group"],
-        ]:
+        if int(TargetAddrMode, 16) in [ ADDRESS_MODE["short"], ADDRESS_MODE["group"], ]:  # uint16_t
             # Short Address
-            TargetNwkId = MsgData[22:26]  # uint16_t
-            Payload = MsgData[26 : len(MsgData)]
+            TargetNwkId = MsgData[22:26]
+            Payload = MsgData[26:]
 
-        elif int(TargetNwkId, 16) == ADDRESS_MODE["ieee"]:  # uint32_t
+        elif int(TargetAddrMode, 16) == ADDRESS_MODE["ieee"]:  # uint32_t
             # IEEE
             TargetNwkId = MsgData[22:38]  # uint32_t
-            Payload = MsgData[38 : len(MsgData)]
+            Payload = MsgData[38:]
 
         else:
             return (None, None, None, None, None, None)
 
     elif int(SrcAddrMode, 16) == ADDRESS_MODE["ieee"]:
         SrcNwkId = MsgData[16:32]  # uint32_t
-        TargetNwkId = MsgData[32:34]
+        TargetAddrMode = MsgData[32:34]
 
-        if int(TargetNwkId, 16) in [
-            ADDRESS_MODE["short"],
-            ADDRESS_MODE["group"],
-        ]:
-            TargetNwkId = MsgData[34:38]  # uint16_t
-            Payload = MsgData[38 : len(MsgData)]
+        if int(TargetAddrMode, 16) in [ ADDRESS_MODE["short"], ADDRESS_MODE["group"], ]:  # uint16_t
+            # Short Address
+            TargetNwkId = MsgData[34:38]
+            Payload = MsgData[38:]
 
-        elif int(TargetNwkId, 16) == ADDRESS_MODE["ieee"]:
+        elif int(TargetAddrMode, 16) == ADDRESS_MODE["ieee"]:  # uint32_t
             # IEEE
-            TargetNwkId = MsgData[34:40]  # uint32_t
-            Payload = MsgData[40 : len(MsgData)]
+            TargetNwkId = MsgData[34:40]
+            Payload = MsgData[40:]
         else:
             return (None, None, None, None, None, None)
     else:
