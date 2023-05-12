f66a3fb97ac1bfde30cb65007e1eafa2702ac206
Patrick Pichon <patrick@pichon.me>
2023-05-12 20:09:35 +0200

    Update Water Counter

--- Modules/tuyaTS0601.py
+++ Modules/tuyaTS0601.py
@@ -370,9 +370,11 @@ def ts0601_smoke_concentration(self, Devices, nwkid, ep, value):
     MajDomoDevice(self, Devices, nwkid, ep, "042a", value)
 
 def ts0601_water_consumption(self, Devices, nwkid, ep, value):
-    self.log.logging("Tuya0601", "Debug", "ts0601_smoke_concentration - Nwkid: %s/%s Consumption: %s" % (nwkid, ep, value))
-    store_tuya_attribute(self, nwkid, "WaterConsumtpion", value)
-    MajDomoDevice(self, Devices, nwkid, ep, "WaterCounter", value)
+    self.log.logging("Tuya0601", "Debug", "ts0601_water_consumption - Nwkid: %s/%s WaterConsumtpion: %s" % (nwkid, ep, value))
+    last_water_consumption = get_tuya_attribute(self, nwkid, 'WaterConsumtpion')
+    current_water_consumption = last_water_consumption + value if last_water_consumption else value
+    store_tuya_attribute(self, nwkid, "WaterConsumtpion", current_water_consumption)
+    MajDomoDevice(self, Devices, nwkid, ep, "WaterCounter", current_water_consumption)
 
 def ts0601_sensor_irrigation_mode(self, Devices, nwkid, ep, value):
     self.log.logging("Tuya0601", "Debug", "ts0601_sensor_irrigation_mode - Nwkid: %s/%s Mode: %s" % (nwkid, ep, value))
