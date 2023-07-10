845bccca68516f9681c03889a362f341ee520323
Patrick Pichon <patrick@pichon.me>
2023-07-10 20:02:11 +0200

    fix issue

--- Zigbee/zclDecoders.py
+++ Zigbee/zclDecoders.py
@@ -41,7 +41,7 @@ def zcl_decoders(self, SrcNwkId, SrcEndPoint, TargetEp, ClusterId, Payload, fram
     fcf = Payload[:2]
     default_response_disable, GlobalCommand, Sqn, ManufacturerCode, Command, Data = retreive_cmd_payload_from_8002(Payload)
     if self.zigbee_communication == "zigpy" and not default_response_disable:
-        if self.pluginconf["enableZclDuplicatecheck"] and self.zigbee_communication == "zigpy" and is_duplicate_zcl_frame(self, SrcNwkId, ClusterId, Sqn):
+        if self.pluginconf.pluginConf["enableZclDuplicatecheck"] and self.zigbee_communication == "zigpy" and is_duplicate_zcl_frame(self, SrcNwkId, ClusterId, Sqn):
             self.log.logging("zclDecoder", "Debug", "zcl_decoders Duplicate frame [%s] %s" %(Sqn, Payload))
             return None
 
