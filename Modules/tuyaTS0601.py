3c2e7d8bfd98a02e166f043e6a465665466b11c1
Patrick Pichon <patrick@pichon.me>
2023-09-14 18:59:55 +0200

    Add ProductionEnergy Widget

--- Modules/tuyaTS0601.py
+++ Modules/tuyaTS0601.py
@@ -293,6 +293,12 @@ def ts0601_summation_energy(self, Devices, nwkid, ep, value):
     checkAndStoreAttributeValue(self, nwkid, ep, "0702", "0000", current_summation)  # Store int
     store_tuya_attribute(self, nwkid, "Energy", value)
 
+def ts0601_production_energy(self, Devices, nwkid, ep, value):
+    self.log.logging( "Tuya0601", "Debug", "ts0601_production_energy - Production Energy %s %s %s" % (nwkid, ep, value), nwkid, )
+    MajDomoDevice(self, Devices, nwkid, ep, "0702", value, Attribute_="0001")
+    checkAndStoreAttributeValue(self, nwkid, ep, "0702", "0001", value)  # Store int
+    store_tuya_attribute(self, nwkid, "ProducedEnergy", value)
+
 
 def ts0601_instant_power(self, Devices, nwkid, ep, value):
     self.log.logging( "Tuya0601", "Debug", "ts0601_instant_power - Instant Power %s %s %s" % (nwkid, ep, value), nwkid, )
@@ -435,6 +441,7 @@ DP_SENSOR_FUNCTION = {
     "mp25": ts0601_mp25,
     "current": ts0601_current,
     "metering": ts0601_summation_energy,
+    "prod_metering": ts0601_production_energy,
     "power": ts0601_instant_power,
     "voltage": ts0601_voltage,
     "heatingstatus": ts0601_heatingstatus,
