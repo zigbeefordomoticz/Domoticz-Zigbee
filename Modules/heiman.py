798b9cd5b92c295df5f0d2c797000480eac84f6a
Patrick Pichon <patrick@pichon.me>
2023-10-15 17:06:45 +0200

    fix Heiman specific command handling

--- Modules/heiman.py
+++ Modules/heiman.py
@@ -23,14 +23,14 @@ def heimanReadRawAPS(self, Devices, nwkId, srcEp, clusterId, targetNwkId, target
 
         command = payLoad[8:10]
         
-    self.log.logging( "Heiman", "Debug", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s" % (
-        nwkId, srcEp, command), nwkId, )
-
-    if int(command, 16) in {0xF0, 0xF1, 0xF2, 0xF3,}:
-        self.log.logging( "Heiman", "Debug", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s UNKNOW" % (
+        self.log.logging( "Heiman", "Debug", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s" % (
             nwkId, srcEp, command), nwkId, )
-        MajDomoDevice(self, Devices, nwkId, "01", "fc80", int(command,16))
-    else:
-        self.log.logging( "Heiman", "Error", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s UNKNOW" % (
-            nwkId, srcEp, command), nwkId, )
+    
+        if int(command, 16) in {0xF0, 0xF1, 0xF2, 0xF3,}:
+            self.log.logging( "Heiman", "Debug", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s UNKNOW" % (
+                nwkId, srcEp, command), nwkId, )
+            MajDomoDevice(self, Devices, nwkId, "01", "fc80", int(command,16))
+        else:
+            self.log.logging( "Heiman", "Error", "heimanReadRawAPS - Nwkid: %s Ep: %s, command found: %s UNKNOW" % (
+                nwkId, srcEp, command), nwkId, )
  
