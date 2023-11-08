94665c68de231b760fca548e9a9061303786e9eb
Patrick Pichon <patrick@pichon.me>
2023-11-08 08:34:06 +0100

    make Fil Pilot using 0xfc00

--- Modules/adeo.py
+++ Modules/adeo.py
@@ -6,6 +6,7 @@ from Modules.tools import get_and_inc_ZCL_SQN
 
 def adeo_fip(self, NwkId, EpOut, fip_level):
     
+    # https://github.com/Koenkk/zigbee2mqtt/issues/19169#issuecomment-1801181411
     ADEO_FIP_ONOFF_COMMAND = {
         "Off": 0,
         "Confort": 1,
@@ -17,9 +18,11 @@ def adeo_fip(self, NwkId, EpOut, fip_level):
     self.log.logging( "Command", "Log", "adeo_fip : Fil Pilote mode: %s - %s" % ( 
         fip_level, ADEO_FIP_ONOFF_COMMAND[fip_level]), NwkId, )
 
-    Cluster = "0006"
-    cluster_frame = 0b00010001
+    Cluster = "fc00"
+    cmd = "00"
+    fcf = "15"
+    manufcode = "128b"
     sqn = get_and_inc_ZCL_SQN(self, NwkId)
-    payload = "%02x" % cluster_frame + sqn + "%02x" % ADEO_FIP_ONOFF_COMMAND[fip_level] 
+    payload = fcf + manufcode[2:4] + manufcode[:2] + sqn + cmd + "%02x" % ADEO_FIP_ONOFF_COMMAND[fip_level]
 
     raw_APS_request(self, NwkId, EpOut, Cluster, "0104", payload, zigpyzqn=sqn, zigate_ep="01", groupaddrmode=False, ackIsDisabled=False)
