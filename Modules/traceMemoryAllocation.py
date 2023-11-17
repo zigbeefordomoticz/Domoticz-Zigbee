2ee6ce8c2456680305a648a31b3ba12d7139ff6f
Patrick Pichon <patrick@pichon.me>
2023-11-17 16:52:23 +0100

    improve tracing facilities

--- Modules/traceMemoryAllocation.py
+++ Modules/traceMemoryAllocation.py
@@ -3,6 +3,7 @@ import re
 import tracemalloc
 from pathlib import Path
 
+import os
 import Domoticz
 
 TOP_LIST = 10
@@ -12,13 +13,13 @@ def start_memory_allocation_tracking(self):
     self.snapshot1 = None
     self.snapshot2 = None
     self.tracemalloc = {}
-    
+
     tracemalloc.start()
     check_memory_allocation(self, "Init")
     
     
 def check_memory_allocation(self, label):
-    
+
     if self.snapshot2:
         self.snapshot1 = self.snapshot2
 
@@ -34,21 +35,7 @@ def top_5_differences(self, label):
     
     top_stats = self.snapshot2.compare_to(self.snapshot1, 'lineno')
     store_in_dictionnary(self, top_stats)
-    if self.log:
-        self.log.logging( "Plugin", "Log", "Top 5 differences at %s" %label)
-    else:
-        Domoticz.Log("Top %s differences at %s" %(TOP_LIST,label))
-
-    for stat in top_stats[:TOP_LIST]:
-        if self.log:
-            self.log.logging( "Plugin", "Log", " - %s" %stat)
-        else:
-            Domoticz.Log( " - %s" %stat)
-    if self.log:
-        self.log.logging( "Plugin", "Log", " " )
-    else:
-        Domoticz.Log(" ")
-        
+    
 
 def store_in_dictionnary(self, tracemalloc_stats):
     # Define a regex pattern to extract relevant information
@@ -57,19 +44,28 @@ def store_in_dictionnary(self, tracemalloc_stats):
     # Parse each line of the tracemalloc logs
     for line in tracemalloc_stats:
         entry = str(line.traceback)
+        if "plugins" not in entry:
+            continue
+        
         size = int(line.size)
         count = (line.count)
 
         if entry in self.tracemalloc:
             MaxSize = self.tracemalloc[ entry ]['MaxSize']
             MaxCount = self.tracemalloc[ entry ]['MaxSize']
+            sizeIncrease = self.tracemalloc[ entry ]['sizeIncrease']
         else:
-            MaxSize = MaxCount =MaxAverage = 0
+            sizeIncrease = MaxSize = MaxCount = 0
 
+        if size > MaxSize:
+            sizeIncrease += 1
         MaxSize = max(size, MaxSize)
         MaxCount = max(count, MaxCount)
+        
+        if 'traceMemoryAllocation' in entry:
+            return
 
-        self.tracemalloc[entry] = {'size': size, 'count': count, 'MaxSize': MaxSize, 'MaxCount': MaxCount}
+        self.tracemalloc[entry] = {'size': size, 'count': count, 'MaxSize': MaxSize, 'MaxCount': MaxCount, 'sizeIncrease': sizeIncrease}
             
             
 def dump_trace_malloc(self):
@@ -79,4 +75,32 @@ def dump_trace_malloc(self):
 
     with open(_tracemalloc_filename, "wt") as file:
         json.dump(self.tracemalloc, file, sort_keys=True, indent=2)
-    self.log.logging("Database", "Debug", "WriteDeviceList - flush Plugin db to %s" % _tracemalloc_filename)
+
+
+def report_top10_allocation(self):
+
+    TOP_nENTRIES = 5 
+    # Tri par MaxSize décroissant, puis par sizeIncrease décroissant
+    sorted_entries_maxsize = sorted(self.tracemalloc.items(), key=lambda x: x[1]['MaxSize'], reverse=True)[:TOP_nENTRIES]
+    sorted_entries_sizeincrease = sorted(self.tracemalloc.items(), key=lambda x: x[1]['sizeIncrease'], reverse=True)[:TOP_nENTRIES]
+
+    # Affichage des n premières entrées avec le MaxSize le plus élevé
+    top10_allocation_data = "Top %s MaxSize " % TOP_nENTRIES
+    for entry in sorted_entries_maxsize:
+        module_name = str(os.path.basename(entry[0]))
+        top10_allocation_data += "| %s | %s | %s | %s" %( module_name, entry[1]['size'], entry[1]['MaxSize'], entry[1]['sizeIncrease'] )
+    log_top10(self,top10_allocation_data)
+        
+    # Affichage des 10 premières entrées avec le sizeIncrease le plus élevé
+    top10_allocation_data = "Top %s sizeIncrease " % TOP_nENTRIES
+    for entry in sorted_entries_sizeincrease:
+        module_name = str(os.path.basename(entry[0]))
+        top10_allocation_data += "| %s | %s | %s | %s" %( module_name, entry[1]['size'], entry[1]['MaxSize'], entry[1]['sizeIncrease'] )
+    log_top10(self,top10_allocation_data)
+    
+def log_top10(self,data):
+ 
+    if self.log:
+        self.log.logging( "Plugin", "Log", "%s" %(data))
+    else:
+        Domoticz.Log("%s" %(data))
