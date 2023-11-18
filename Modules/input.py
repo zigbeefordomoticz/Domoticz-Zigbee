06ed3cd4f234d319730c852d3cc9e5ba9c6685e1
Patrick Pichon <patrick@pichon.me>
2023-11-18 17:48:35 +0100

    prevent handling a group message when group is not yet ready ( https://www.domoticz.com/forum/viewtopic.php?p=308992#p308992 )

--- Modules/input.py
+++ Modules/input.py
@@ -2501,22 +2501,26 @@ def Decode804E(self, Devices, MsgData, MsgLQI):
 # Implemented in GroupMgtv2.GrupManagement.py
 def Decode8060(self, Devices, MsgData, MsgLQI):
 
-    self.groupmgt.add_group_member_ship_response(MsgData)
+    if self.groupmgt:
+        self.groupmgt.add_group_member_ship_response(MsgData)
 
 
 def Decode8061(self, Devices, MsgData, MsgLQI):
 
-    self.groupmgt.check_group_member_ship_response(MsgData)
+    if self.groupmgt:
+        self.groupmgt.check_group_member_ship_response(MsgData)
 
 
 def Decode8062(self, Devices, MsgData, MsgLQI):
 
-    self.groupmgt.look_for_group_member_ship_response(MsgData)
+    if self.groupmgt:
+        self.groupmgt.look_for_group_member_ship_response(MsgData)
 
 
 def Decode8063(self, Devices, MsgData, MsgLQI):
 
-    self.groupmgt.remove_group_member_ship_response(MsgData)
+    if self.groupmgt:
+        self.groupmgt.remove_group_member_ship_response(MsgData)
 
 
 # Reponses SCENE
