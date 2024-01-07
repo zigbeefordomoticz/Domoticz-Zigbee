f13a3df041d2751decde4c3c9b271386aabaebc8
Patrick Pichon <patrick@pichon.me>
2024-01-07 18:18:45 +0100

    make and Error message to Log level

--- Z4D_decoders/z4d_decoder_IEEE_Addr_Rsp.py
+++ Z4D_decoders/z4d_decoder_IEEE_Addr_Rsp.py
@@ -26,27 +26,27 @@ def Decode8041(self, Devices, MsgData, MsgLQI):
         self.log.logging('Input', 'Debug', 'Decode8041 - IEEE Address response, Sequence number: %s Status: %s IEEE: %s  NwkId: %s nbAssociated Devices: %s StartIdx: %s DeviceList: %s' % (MsgSequenceNumber, DisplayStatusCode(MsgDataStatus), MsgIEEE, MsgShortAddress, MsgNumAssocDevices, MsgStartIndex, MsgDeviceList))
 
     if MsgShortAddress == '0000' and self.ControllerIEEE and (MsgIEEE != self.ControllerIEEE):
-        self.log.logging('Input', 'Error', 'Decode 8041 - Receive an IEEE: %s with a NwkId: %s something wrong !!!' % (MsgIEEE, MsgShortAddress))
+        self.log.logging('Input', 'Error', 'Decode8041 - Receive an IEEE: %s with a NwkId: %s something wrong !!!' % (MsgIEEE, MsgShortAddress))
         return
 
     elif self.ControllerIEEE and MsgIEEE == self.ControllerIEEE and (MsgShortAddress != '0000'):
-        self.log.logging('Input', 'Log', 'Decode 8041 - Receive an IEEE: %s with a NwkId: %s something wrong !!!' % (MsgIEEE, MsgShortAddress))
+        self.log.logging('Input', 'Log', 'Decode8041 - Receive an IEEE: %s with a NwkId: %s something wrong !!!' % (MsgIEEE, MsgShortAddress))
         return
 
     if MsgShortAddress in self.ListOfDevices and 'IEEE' in self.ListOfDevices[MsgShortAddress] and (self.ListOfDevices[MsgShortAddress]['IEEE'] == MsgShortAddress):
-        self.log.logging('Input', 'Debug', 'Decode 8041 - Receive an IEEE: %s with a NwkId: %s' % (MsgIEEE, MsgShortAddress))
+        self.log.logging('Input', 'Debug', 'Decode8041 - Receive an IEEE: %s with a NwkId: %s' % (MsgIEEE, MsgShortAddress))
         timeStamped(self, MsgShortAddress, 32833)
         loggingMessages(self, '8041', MsgShortAddress, MsgIEEE, MsgLQI, MsgSequenceNumber)
         lastSeenUpdate(self, Devices, NwkId=MsgShortAddress)
         return
 
     if MsgIEEE in self.IEEE2NWK:
-        self.log.logging('Input', 'Debug', 'Decode 8041 - Receive an IEEE: %s with a NwkId: %s, will try to reconnect' % (MsgIEEE, MsgShortAddress))
+        self.log.logging('Input', 'Debug', 'Decode8041 - Receive an IEEE: %s with a NwkId: %s, will try to reconnect' % (MsgIEEE, MsgShortAddress))
 
         if not DeviceExist(self, Devices, MsgShortAddress, MsgIEEE):
             if not zigpy_plugin_sanity_check(self, MsgShortAddress):
                 handle_unknow_device(self, MsgShortAddress)
-            self.log.logging('Input', 'Log', 'Decode 8041 - Not able to reconnect (unknown device) %s %s' % (MsgIEEE, MsgShortAddress))
+            self.log.logging('Input', 'Log', 'Decode8041 - Not able to reconnect (unknown device) %s %s' % (MsgIEEE, MsgShortAddress))
             return
 
         timeStamped(self, MsgShortAddress, 32833)
@@ -54,4 +54,4 @@ def Decode8041(self, Devices, MsgData, MsgLQI):
         lastSeenUpdate(self, Devices, NwkId=MsgShortAddress)
         return
 
-    self.log.logging('Input', 'Error', 'Decode 8041 - Receive an IEEE: %s with a NwkId: %s, seems not known by the plugin' % (MsgIEEE, MsgShortAddress))
+    self.log.logging('Input', 'Log', 'WARNING - Decode8041 - Receive an IEEE: %s with a NwkId: %s, not known by the plugin' % (MsgIEEE, MsgShortAddress))
