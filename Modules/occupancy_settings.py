1f7275c7a5d82464156362c6ed8891cbe4c224f4
Patrick Pichon <patrick@pichon.me>
2024-01-31 14:42:35 +0100

    minor fixes

--- Modules/occupancy_settings.py
+++ Modules/occupancy_settings.py
@@ -14,7 +14,7 @@ from Modules.basicOutputs import write_attribute
 from Modules.develco import is_develco_device
 from Modules.sonoff import is_sonoff_device, sonoff_set_ultrasonic_occupancySensibility
 from Modules.philips import is_philips_device, philips_set_pir_occupancySensibility
-from Modules.readAttributes import ReadAttributeRequest_0406_0010, ReadAttributeRequest_0406_0020
+from Modules.readAttributes import ReadAttributeRequest_0406_0010, ReadAttributeRequest_0406_0012, ReadAttributeRequest_0406_0020, ReadAttributeRequest_0406_0022
 from Modules.tools import getListOfEpForCluster
 from Modules.zigateConsts import ZIGATE_EP
 
@@ -63,6 +63,7 @@ def current_PIR_OccupiedToUnoccupied_Delay(self, nwkid, ep):
     else:
         return None
 
+
 def PIR_unoccupied_to_occupied_delay(self, nwkid, ep, value):
     self.log.logging( "BasicOutput", "Log", f"PIR_unoccupied_to_occupied_delay for {nwkid}/{ep} - delay: {value}", nwkid )
 
@@ -95,6 +96,16 @@ def PIR_unoccupied_to_occupied_threshold(self, nwkid, ep, value):
         "%02x" %PIR_CONFIG_SET[ "PIRUnoccupiedToOccupiedThreshold"][1], 
         "%02x" %value, 
         ackIsDisabled=False, )
+
+
+def current_PIR_OccupiedToUnoccupied_Threshold(self, nwkid, ep):
+    ep_data = self.ListOfDevices.get(nwkid, {}).get("Ep", {}).get(ep, {}).get(OCCUPANCY_CLUSTER_ID, {})
+    attribute_0012 = ep_data.get("0012", None)
+
+    if attribute_0012 is not None:
+        return int(attribute_0012, 16) if isinstance(attribute_0012, str) else attribute_0012
+
+    return None
 
 
 def Ultrasonic_occupied_to_unoccupied_delay(self, nwkid, ep, value):
@@ -148,6 +159,15 @@ def Ultrasonic_unoccupied_to_occupied_threshold(self, nwkid, ep, value):
         ackIsDisabled=False, )
 
 
+def current_Ultrasonic_unoccupied_to_occupied_threshold(self, nwkid, ep):
+    ep_data = self.ListOfDevices.get(nwkid, {}).get("Ep", {}).get(ep, {}).get(OCCUPANCY_CLUSTER_ID, {})
+    attribute_0022 = ep_data.get("0022", None)
+
+    if attribute_0022 is not None:
+        return int(attribute_0022, 16) if isinstance(attribute_0022, str) else attribute_0022
+
+    return None
+
 # paramDevice helpers
 
 def common_PIROccupiedToUnoccupiedDelay(self, nwkid, delay):
@@ -169,7 +189,29 @@ def common_PIROccupiedToUnoccupiedDelay(self, nwkid, delay):
         current_delay = current_PIR_OccupiedToUnoccupied_Delay(self, nwkid, ep)
         if current_delay != delay:
             PIR_occupied_to_unoccupied_delay(self, nwkid, ep, delay)
-            ReadAttributeRequest_0406_0010(self, nwkid)
+    ReadAttributeRequest_0406_0010(self, nwkid)
+
+
+def common_PIR_occupancySensibility(self, nwkid, sensibility):
+    self.log.logging( "BasicOutput", "Log", f"common_PIR_occupancySensibility for {nwkid} - sensibility: {sensibility}", nwkid )
+    
+    if is_philips_device(self, nwkid):
+        philips_set_pir_occupancySensibility(self, nwkid, sensibility)
+        return
+
+    elif is_develco_device(self, nwkid):
+        ListOfEp = ["22", "28", "29"]
+        
+    else:
+        ListOfEp = getListOfEpForCluster(self, nwkid, OCCUPANCY_CLUSTER_ID)
+        
+    self.log.logging( "BasicOutput", "Log", f"common_Ultrasonic_occupancySensibility for {nwkid} - delay: {sensibility} found Endpoint {ListOfEp}", nwkid )
+    for ep in ListOfEp:
+        current_sensibility = current_PIR_OccupiedToUnoccupied_Threshold(self, nwkid, ep)
+        if current_sensibility != sensibility:
+            PIR_unoccupied_to_occupied_threshold(self, nwkid, ep, sensibility)
+    ReadAttributeRequest_0406_0012(self, nwkid)
+
 
 def common_Ultrasnonic_OccupiedToUnoccupiedDelay(self, nwkid, delay):
     self.log.logging( "BasicOutput", "Log", f"common_Ultrasnonic_OccupiedToUnoccupiedDelay for {nwkid} - delay: {delay}", nwkid )
@@ -182,19 +224,24 @@ def common_Ultrasnonic_OccupiedToUnoccupiedDelay(self, nwkid, delay):
         current_delay = current_PIR_OccupiedToUnoccupied_Delay(self, nwkid, ep)
         if current_delay != delay:
             Ultrasonic_occupied_to_unoccupied_delay(self, nwkid, ep, delay)
-            ReadAttributeRequest_0406_0020(self, nwkid)
-   
+    ReadAttributeRequest_0406_0020(self, nwkid)
 
-def common_PIR_occupancySensibility(self, nwkid, sensibility):
-    self.log.logging( "BasicOutput", "Log", f"common_PIR_occupancySensibility for {nwkid} - sensibility: {sensibility}", nwkid )
-    
-    if is_philips_device(self, nwkid):
-        philips_set_pir_occupancySensibility(self, nwkid, sensibility)
 
 def common_Ultrasonic_occupancySensibility(self, nwkid, sensibility):
     self.log.logging( "BasicOutput", "Log", f"common_Ultrasonic_occupancySensibility for {nwkid} - sensibility: {sensibility}", nwkid )
     
+    # Determine EndPoints
     if is_sonoff_device(self, nwkid):
-        sonoff_set_ultrasonic_occupancySensibility(self, nwkid, sensibility)
-    
+        ListOfEp = ["01",]
+
+    else:
+        ListOfEp = getListOfEpForCluster(self, nwkid, OCCUPANCY_CLUSTER_ID)
+        
+    self.log.logging( "BasicOutput", "Log", f"common_Ultrasonic_occupancySensibility for {nwkid} - delay: {sensibility} found Endpoint {ListOfEp}", nwkid )
+    for ep in ListOfEp:
+        current_sensibility = current_Ultrasonic_unoccupied_to_occupied_threshold(self, nwkid, ep)
+        if current_sensibility != sensibility:
+            Ultrasonic_unoccupied_to_occupied_threshold(self, nwkid, ep, sensibility)
+    ReadAttributeRequest_0406_0022(self, nwkid)
+
     
