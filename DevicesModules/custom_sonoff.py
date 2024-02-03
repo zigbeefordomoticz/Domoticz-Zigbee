9e65d1de31412789eb97e49ddfd8f03e97af2c37
Patrick Pichon <patrick@pichon.me>
2024-02-03 11:41:51 +0100

    remove Illumination

--- DevicesModules/custom_sonoff.py
+++ DevicesModules/custom_sonoff.py
@@ -33,17 +33,7 @@ def sonoff_open_window_detection(self, nwkid, detection):
     self.log.logging("Sonoff", "Debug", "sonoff_child_lock - Nwkid: %s Mode: %s" %(nwkid, detection))
     write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", "6000", "10", "%02x" %detection, ackIsDisabled=False)
 
-
-def sonoff_ultrasonic_illumination(self, nwkid, mode):
-    self.log.logging("Sonoff", "Error", "sonoff_ultrasonic_illumination - Nwkid: %s Mode: %s (DISABLED)" %(nwkid, mode))
-    #self.log.logging("Sonoff", "Debug", "sonoff_ultrasonic_illumination - Nwkid: %s Mode: %s" %(nwkid, mode))
-    #write_attribute( self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_ILLUMINATION_ATTRIBUTE, "20", "%02x" % mode, ackIsDisabled=False)
-    #ReadAttributeRequest_0406_0022(self, nwkid)
-
-
 SONOFF_DEVICE_PARAMETERS = {
     "SonOffTRVChildLock": sonoff_child_lock,
     "SonOffTRVWindowDectection": sonoff_open_window_detection,
-    "SonOffUltrasonicIllumination": sonoff_ultrasonic_illumination
-
 }
