2f0deba7954c63c45bbc51776d7c0af789519ce8
Pipiche <8291674+pipiche38@users.noreply.github.com>
2024-02-26 13:38:53 +0100

    SetParams do not provide Ep
    Fix issue described here https://easydomoticz.com/forum/viewtopic.php?t=13910&start=10

--- Modules/ias_settings.py
+++ Modules/ias_settings.py
@@ -22,8 +22,14 @@ ONOFF_CONFIG_SET = {
     "CurrentZoneSensitivityLevel": ( "0013", "20")
 }
 
+def ias_CurrentZoneSensitivityLevel(self, nwkid, value):
+    self.log.logging( "onoffSettings", "Debug", f"ias_CurrentZoneSensitivityLevel for {nwkid} - sensitivity: {value}", nwkid )
+    ListOfEp = getListOfEpForCluster(self, nwkid, IAS_CLUSTER_ID)
+    for ep in ListOfEp:
+        ias_CurrentZoneSensitivityLevel_by_ep(self, nwkid, ep, value)
 
-def ias_CurrentZoneSensitivityLevel(self, nwkid, ep, value):
+
+def ias_CurrentZoneSensitivityLevel_by_ep(self, nwkid, ep, value):
     """ Allows an IAS Zone client to query and configure the IAS Zone server’s sensitivity level. """
     
     # The default value 0x00 is the device’s default sensitivity level as configured by the manufacturer. It MAY
