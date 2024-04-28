e3be31893bb6f3ed77684891f4f047646174f5ff
Patrick Pichon <patrick@pichon.me>
2024-04-28 10:27:20 +0200

    Refactor

--- Modules/checkingUpdate.py
+++ Modules/checkingUpdate.py
@@ -1,17 +1,22 @@
 #!/usr/bin/env python3
-# coding: utf-8 -*-
+# -*- coding: utf-8 -*-
 #
-# Author: zaraki673 & pipiche38
+# Implementation of Zigbee for Domoticz plugin.
 #
-
-# Enable Version Check ( will required Internet connectivity )
+# This file is part of Zigbee for Domoticz plugin. https://github.com/zigbeefordomoticz/Domoticz-Zigbee
+# (C) 2015-2024
+#
+# Initial authors: zaraki673 & pipiche38
+#
+# SPDX-License-Identifier:    GPL-3.0 license
 
 # Use DNS TXT to check latest version  available on gitHub
 # - stable
 # - beta
 
-# Provide response to REST API request
+
 import dns.resolver
+import requests
 
 PLUGIN_TXT_RECORD = "zigate_plugin.pipiche.net"
 ZIGATEV1_FIRMWARE_TXT_RECORD = "zigatev1.pipiche.net"
@@ -25,42 +30,61 @@ ZIGATE_DNS_RECORDS = {
 }
 
 
-def checkPluginVersion(self, zigbee_communitation, branch, zigate_model):
-
-    TXT_RECORD = None
-    if zigbee_communitation == "native":
-        TXT_RECORD = ZIGATE_DNS_RECORDS.get(zigate_model)
-
-    zigate_plugin = zigateVersions = None
+def check_plugin_version_against_dns(self, zigbee_communication, branch, zigate_model):
+    self.log.logging("Plugin", "Debug", f"check_plugin_version_against_dns {zigbee_communication} {branch} {zigate_model}")
+    
     try:
-        zigate_plugin = dns.resolver.resolve(PLUGIN_TXT_RECORD, "TXT", tcp=True, lifetime=1).response.answer[0]
-        zigate_plugin = str( zigate_plugin[0] ).strip('"')
-        if TXT_RECORD:
-            zigateVersions = dns.resolver.resolve(TXT_RECORD, "TXT", tcp=True, lifetime=1).response.answer[0]
-            zigateVersions = str(zigateVersions[0]).strip('"')
+        plugin_version = None
+        firmware_version = None
+
+        plugin_version = _get_dns_txt_record(self, PLUGIN_TXT_RECORD)
+        plugin_version_dict = _parse_dns_txt_record( plugin_version)
+        
+        # If native communication (zigate) let's find the zigate firmware
+        if zigbee_communication == "native":
+            zigate_plugin_record = ZIGATE_DNS_RECORDS.get(zigate_model)
+            firmware_version = _get_dns_txt_record(self, zigate_plugin_record)
+            firmware_version_dict = _parse_dns_txt_record(firmware_version)
+            self.log.logging("Plugin", "Debug", f"check_plugin_version_against_dns {firmware_version} {firmware_version_dict}")
+            
+        self.log.logging("Plugin", "Debug", f"check_plugin_version_against_dns {plugin_version} {plugin_version_dict}")
+        
+        if zigbee_communication == "native" and branch in plugin_version_dict and "firmMajor" in firmware_version_dict and "firmMinor" in firmware_version_dict:
+            return (plugin_version_dict[branch], firmware_version_dict["firmMajor"], firmware_version_dict["firmMinor"])
+    
+        if zigbee_communication == "zigpy" and branch in plugin_version_dict:
+            return (plugin_version_dict[branch], 0, 0)
+    
+        self.log.logging("Plugin", "Error", f"You are running {branch}-{plugin_version}, a NOT SUPPORTED version. "
+                         "Please refer to https://github.com/zigbeefordomoticz/Domoticz-Zigbee to get the latest information")
+
     except Exception as e:
-        return (0, 0, 0)
+        self.log.logging("Plugin", "Error", f"An error occurred while checking plugin version: {e}")
+        if not is_internet_available():
+            self.internet_available = False
 
-    pluginVersion = {}
-    if zigate_plugin and zigate_plugin != "":
-        for branch_version in zigate_plugin.split(";"):
-            pluginVersion[branch_version.split("=")[0]] = branch_version.split("=")[1].strip('"')
-
-    firmwareVersion = {}
-    if zigateVersions and zigateVersions != "":
-        for major_minor in zigateVersions.split(";"):
-            firmwareVersion[major_minor.split("=")[0]] = major_minor.split("=")[1].strip('"')
-
-    if zigbee_communitation == "native" and branch in pluginVersion and "firmMajor" in firmwareVersion and "firmMinor" in firmwareVersion:
-        return (pluginVersion[branch], firmwareVersion["firmMajor"], firmwareVersion["firmMinor"])
-    if zigbee_communitation == "zigpy" and branch in pluginVersion:
-        return (pluginVersion[branch], 0, 0)
-
-    self.log.logging("Plugin", "Error", "You are running %s-%s , a NOT SUPPORTED version. Please refer to https://github.com/zigbeefordomoticz/Domoticz-Zigbee to get the latest informations" % (branch, pluginVersion ))
     return (0, 0, 0)
 
 
-def checkPluginUpdate(self, currentVersion, availVersion):
+def _get_dns_txt_record(self, record):
+    try:
+        result = dns.resolver.resolve(record, "TXT", tcp=True, lifetime=1).response.answer[0]
+        return str(result[0]).strip('"')
+
+    except Exception as e:
+        self.log.logging("Plugin", "Error", f"An error occurred while resolving DNS TXT record: {e}")
+        return None
+
+
+def _parse_dns_txt_record(txt_record):
+    version_dict = {}
+    if txt_record and txt_record != "":
+        for branch_version in txt_record.split(";"):
+            version_dict.update({k.strip(): v.strip('"') for k, v in (item.split("=") for item in branch_version.split(";"))})
+    return version_dict
+
+
+def is_plugin_update_available(self, currentVersion, availVersion):
     if availVersion == 0:
         return False
 
@@ -68,22 +92,32 @@ def checkPluginUpdate(self, currentVersion, availVersion):
     availMaj, availMin, availUpd = availVersion.split(".")
 
     if availMaj > currentMaj:
-        self.log.logging("Plugin", "Status", "checkPluginVersion - Upgrade available: %s" %availVersion)
+        self.log.logging("Plugin", "Status", "Zigbee4Domoticz plugin:  upgrade available: %s" %availVersion)
         return True
+
     if availMaj == currentMaj and (
         availMin == currentMin
         and availUpd > currentUpd
         or availMin > currentMin
     ):
-        self.log.logging("Plugin", "Status", "checkPluginVersion - Upgrade available: %s" %availVersion)
+        self.log.logging("Plugin", "Status", "Zigbee4Domoticz plugin:  upgrade available: %s" %availVersion)
         return True
     return False
 
 
-def checkFirmwareUpdate(self, currentMajorVersion, currentFirmwareVersion, availfirmMajor, availfirmMinor):
+def is_zigate_firmware_available(self, currentMajorVersion, currentFirmwareVersion, availfirmMajor, availfirmMinor):
     if not (availfirmMinor and currentFirmwareVersion):
         return False
     if int(availfirmMinor, 16) > int(currentFirmwareVersion, 16):
-        self.log.logging("Plugin", "Status", "checkFirmwareUpdate - Firmware update available")
+        self.log.logging("Plugin", "Status", "Zigate Firmware update available")
         return True
     return False
+
+
+def is_internet_available():
+    try:
+        response = requests.get("http://www.google.com", timeout=3)
+        # Check if the status code is a success code (2xx)
+        return response.status_code == 200
+    except requests.ConnectionError:
+        return False
