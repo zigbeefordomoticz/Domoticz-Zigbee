4c77406f6047220a6da350f2bbb8925ee8a9d311
Pipiche <8291674+pipiche38@users.noreply.github.com>
2024-07-05 23:53:07 +0200

    swithc debug to log to get more insights

--- DevicesModules/custom_sunricher.py
+++ DevicesModules/custom_sunricher.py
@@ -29,21 +29,21 @@ def is_sunricher_device(self, nwkid):
 
 
 def sunricher_pir_sensor_sensitivity(self, nwkid, sensitivity):
-    self.log.logging("Sunricher", "Debug", "sunricher_pir_sensor_sensitivity - Nwkid: %s Mode: %s" % (nwkid, sensitivity))
+    self.log.logging("Sunricher", "Log", "sunricher_pir_sensor_sensitivity - Nwkid: %s Mode: %s" % (nwkid, sensitivity))
     write_attribute(self, nwkid, ZIGATE_EP, "01", OCCUPANCY_CLUSTERID, SUNRICHER_MANUFACTURER_ID, "01", SUNRICHER_PIR_SENSOR_SENSITIVITY, "30", "%02x" %sensitivity, ackIsDisabled=False)
 
 
 def sunricher_motion_detection_blind_time(self, nwkid, blind_time):
-    self.log.logging("Sunricher", "Debug", "sunricher_motion_detection_blind_time - Nwkid: %s Mode: %s" %(nwkid, blind_time))
+    self.log.logging("Sunricher", "Log", "sunricher_motion_detection_blind_time - Nwkid: %s Mode: %s" %(nwkid, blind_time))
     write_attribute(self, nwkid, ZIGATE_EP, "01", OCCUPANCY_CLUSTERID, SUNRICHER_MANUFACTURER_ID, "01", SUNRICHER_MOTION_DETECTION_BLIND_TIME, "20", "%02x" %blind_time, ackIsDisabled=False)
 
 
 def sunricher_motion_detection_pulse_counter(self, nwkid, pulse_setting):
-    self.log.logging("Sunricher", "Debug", "sunricher_motion_detection_pulse_counter - Nwkid: %s Mode: %s" %(nwkid, pulse_setting))
+    self.log.logging("Sunricher", "Log", "sunricher_motion_detection_pulse_counter - Nwkid: %s Mode: %s" %(nwkid, pulse_setting))
     write_attribute(self, nwkid, ZIGATE_EP, "01", OCCUPANCY_CLUSTERID, SUNRICHER_MANUFACTURER_ID, "01", SUNRICHER_MOTION_DETECTION_PULSE_COUNTER, "30", "%02x" %pulse_setting, ackIsDisabled=False)
 
 def sunricher_pir_sensor_trigger_time_interval(self, nwkid, trigger_time_interval):
-    self.log.logging("Sunricher", "Debug", "sunricher_pir_sensor_trigger_time_interval - Nwkid: %s Mode: %s" %(nwkid, sunricher_pir_sensor_trigger_time_interval))
+    self.log.logging("Sunricher", "Log", "sunricher_pir_sensor_trigger_time_interval - Nwkid: %s Mode: %s" %(nwkid, sunricher_pir_sensor_trigger_time_interval))
     write_attribute(self, nwkid, ZIGATE_EP, "01", OCCUPANCY_CLUSTERID, SUNRICHER_MANUFACTURER_ID, "01", SUNRICHER_PIR_SENSOR_TRIGGER_TIME_INTERVAL, "30", "%02x" %trigger_time_interval, ackIsDisabled=False)
 
 
