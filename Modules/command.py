cc9ebf5bff6ee58c1942492a5b1c2c066fab3ccd
Patrick Pichon <patrick@pichon.me>
2024-08-17 09:44:16 +0200

    fix codefactor - 2 spaces

--- Modules/command.py
+++ Modules/command.py
@@ -419,7 +419,7 @@ def handle_command_off(self,Devices, DeviceID, Unit, Level, Nwkid, EPout, Device
         if model_name in ( "PR412", "CPR412", "CPR412-E"):
             actuator_off(self, Nwkid, EPout, "Light")
 
-        elif  DeviceType in ( "Curtain", ) and ts0601_extract_data_point_infos( self, model_name):
+        elif DeviceType in ( "Curtain", ) and ts0601_extract_data_point_infos( self, model_name):
             ts0601_actuator(self, Nwkid, "CurtainState", 0)
             update_domoticz_widget(self, Devices, DeviceID, Unit, 0, "Off", BatteryLevel, SignalLevel, ForceUpdate_=forceUpdateDev)
             return
