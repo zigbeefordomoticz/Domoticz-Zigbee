3638cf68edbf4bf8ff6ff0ba04e2dc1fd049e6f8
Patrick Pichon <patrick@pichon.me>
2024-09-08 11:30:26 +0200

    prioritize with timestamp to take Percentage or Voltage

--- Modules/batterieManagement.py
+++ Modules/batterieManagement.py
@@ -17,7 +17,7 @@
 
 """
 
-from time import time
+import time
 
 from Modules.domoTools import Update_Battery_Device
 from Modules.tools import get_deviceconf_parameter_value, voltage2batteryP
@@ -43,26 +43,28 @@ def UpdateBatteryAttribute(self, Devices, MsgSrcAddr, MsgSrcEp):
     # Compute Battery %
     mainVolt = battVolt = battRemainingVolt = battRemainPer = None
 
-    device_data = self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp]["0001"]
+    device_power_cluster = self.ListOfDevices[MsgSrcAddr]["Ep"][MsgSrcEp]["0001"]
 
-    mainVolt = get_float_value(device_data, ["0000"])
-    battVolt = get_float_value(device_data, ["0010"])
-    battRemainingVolt = get_float_value(device_data, ["0020"])
-    battRemainPer = get_float_value(device_data, ["0021"])
+    mainVolt = get_float_value(device_power_cluster, ["0000"])
+    battVolt = get_float_value(device_power_cluster, ["0010"])
+    battRemainingVolt = get_float_value(device_power_cluster, ["0020"])
+    battRemainPer = get_float_value(device_power_cluster, ["0021"])
 
+    battery_percengtage_timestamp = self.ListOfDevices[MsgSrcAddr].get("BatteryPercentage_TimeStamp", 0)
+    battery_voltage_timestamp = self.ListOfDevices[MsgSrcAddr].get("BatteryVoltage_TimeStamp", 0)
 
     value = None
     # Based on % ( 0x0021 )
-    if battRemainPer:
+    if battRemainPer and battery_percengtage_timestamp >= battery_voltage_timestamp:
         value = battRemainPer
-        BatteryPercentageConverter = get_deviceconf_parameter_value(self, self.ListOfDevices[MsgSrcAddr]["Model"], "BatteryPercentageConverter")
+        BatteryPercentageConverter = get_deviceconf_parameter_value(self, model_name, "BatteryPercentageConverter")
         if BatteryPercentageConverter:
             value = round(value / BatteryPercentageConverter)
 
     # Based on Remaining Voltage
-    elif battRemainingVolt:
-        MaxBatteryVoltage = get_deviceconf_parameter_value(self, self.ListOfDevices[MsgSrcAddr]["Model"], "MaxBatteryVoltage", 30)
-        MinBatteryVoltage = get_deviceconf_parameter_value(self, self.ListOfDevices[MsgSrcAddr]["Model"], "MinBatteryVoltage", 25)
+    elif battRemainingVolt and battery_voltage_timestamp >= battery_percengtage_timestamp:
+        MaxBatteryVoltage = get_deviceconf_parameter_value(self, model_name, "MaxBatteryVoltage", 30)
+        MinBatteryVoltage = get_deviceconf_parameter_value(self, model_name, "MinBatteryVoltage", 25)
         value = voltage2batteryP(battRemainingVolt, MaxBatteryVoltage, MinBatteryVoltage)
 
     self.log.logging( ["Cluster", "BatteryManagement"], "Debug", f'UpdateBatteryAttribute - Device: {MsgSrcAddr} Model: {model_name} mainVolt:{mainVolt} , battVolt:{battVolt}, battRemainingVolt: {battRemainingVolt}, battRemainPer:{battRemainPer} => value: {value} ', MsgSrcAddr, )
@@ -70,7 +72,7 @@ def UpdateBatteryAttribute(self, Devices, MsgSrcAddr, MsgSrcEp):
     if value is None:
         return
 
-    self.ListOfDevices[MsgSrcAddr]["BatteryUpdateTime"] = int(time())
+    self.ListOfDevices[MsgSrcAddr]["BatteryUpdateTime"] = int(time.time())
     if value != self.ListOfDevices[MsgSrcAddr]["Battery"]:
         self.ListOfDevices[MsgSrcAddr]["Battery"] = value
         Update_Battery_Device(self, Devices, MsgSrcAddr, value)
