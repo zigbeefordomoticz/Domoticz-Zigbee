79aab806c46e4aacf7a75105cdb9ed62a8588319
Patrick Pichon <patrick@pichon.me>
2024-10-29 13:20:21 +0100

    forward Cluster 0x0019 packets to zigpy layer to process OTA

--- Classes/ZigpyTransport/AppGeneric.py
+++ Classes/ZigpyTransport/AppGeneric.py
@@ -408,6 +408,11 @@ def packet_received(
         super(type(self),self).packet_received(packet)
         return
 
+    if cluster == 0x0019:
+        # forward message to zigpy
+        super(type(self),self).packet_received(packet)
+        return
+
     packet.lqi = 0x00 if packet.lqi is None else packet.lqi
     profile = 0x0000 if src_ep == dst_ep == 0x00 else profile
 
@@ -418,10 +423,6 @@ def packet_received(
     plugin_frame = build_plugin_8002_frame_content(self, sender, profile, cluster, src_ep, dst_ep, message, packet.lqi, src_addrmode=addr_mode)
     self.log.logging("TransportZigpy", "Debug", "packet_received Sender: %s frame for plugin: %s" % (sender, plugin_frame))
     self.callBackFunction(plugin_frame)
-
-    if cluster == 0x0019:
-        # Do not forward message to zigpy
-        return
 
     super(type(self),self).packet_received(packet)
     
