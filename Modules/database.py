eb5a55283c7c9a6c5a7d6c88f070b6df4b9bec42
Patrick Pichon <patrick@pichon.me>
2024-10-30 11:50:08 +0100

    remove extra log

--- Modules/database.py
+++ Modules/database.py
@@ -233,7 +233,6 @@ def perform_cleanup_and_validation(self, nwkid):
         func, condition = action[:2]
         args = action[2:] if len(action) > 2 else []
         if condition:
-            self.log.logging("Database", "Log", f"{action} - {condition} -> {nwkid} {str(*args)}")
             func(self, nwkid, *args)
 
     # Log additional reset if needed
