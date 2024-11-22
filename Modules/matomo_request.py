01c248a74a185eb5c37dff8c0ce89a224dac8132
Patrick Pichon <patrick@pichon.me>
2024-11-22 11:36:58 +0100

    Witch to a new matomo test SiteId

--- Modules/matomo_request.py
+++ Modules/matomo_request.py
@@ -1,19 +1,22 @@
 import hashlib
 import json
+import os
 import platform
+import re
 import sys
 import time
 
+import distro
 import requests
 
-# Matomo endpoint and authentication details
+# Matomo endpoint details
 MATOMO_URL = "https://z4d.pipiche.net/matomo.php"
-APIV = 1
-SITE_ID = 8  # 8 for Testing , 7 for Production
+APIV = 1  # API Version
+SITE_ID = 9   # 7 for Production
 ACTION_NAME = "PluginInstanceInfos"
-RONELABS_MODEL_INFO = "/etc/modelinfo"
 
-# Custom Variables (up to 5 allowed per request)
+RONELABS_MODEL_INFO = "/etc/modelinfo"
+DEVICE_TREE_CONFIGURATION = "/proc/device-tree/model"
 
 
 def get_clientid(self):
@@ -26,52 +29,80 @@ def get_clientid(self):
     mac_address = self.ListOfDevices.get('0000', {}).get('IEEE', None)
     if mac_address:
         return hashlib.sha256(mac_address.encode()).hexdigest()
-    
+
     return None
 
 
-def populate_custom_variables_pack1(self):
-
-    return {
-        "1" : [ "DomoticzVersion", self.pluginParameters.get("DomoticzVersion") ],
-        "2" : [ "CoordinatorModel", self.pluginParameters.get("CoordinatorModel") ],
-        "3" : [ "PluginVersion", self.pluginParameters.get("PluginVersion") ],
-        "4" : [ "CoordinatorFirmwareversion", self.pluginParameters.get("DisplayFirmwareVersion") ],
-        "5" : [ "NetworkSize", self.pluginParameters.get("NetworkSize") ],
-    }
-
-
-def populate_custom_variables_pack2(self):
-    return {
-        "6" : [ "NetworkDevices", self.pluginParameters.get("NetworkDevices") ],
-        "7" : [ "CertifiedDbVersion", self.pluginParameters.get("CertifiedDbVersion") ],
-        "8" : [ "PlatformDistribution", str( get_os_info()) ],
-        "9" : [ "ArchitectureInformation", str( get_architecture_model() )],
-        "10": [ "Uptime", str( time.time() - self.statistics._start) ],
-    }
-
-
 def populate_custom_dimmensions(self):
-    
-    _custom_dimmensions = {}
-    _ronlabs_model = get_ronelabs_model_custom_definition()
-    if _ronlabs_model:
-        _custom_dimmensions[ _ronlabs_model[0] ] = _ronlabs_model[1]
 
-    return _custom_dimmensions
+    _custom_dimensions = { }
+
+    # Domoticz version
+    _domo = self.pluginParameters.get("DomoticzVersion")
+    if _domo:
+        _custom_dimensions[ "dimension1"] = clean_custom_dimension_value( _domo)
+
+    # Coordinator Model
+    _coordinator_model = self.pluginParameters.get("CoordinatorModel")
+    if _coordinator_model:
+        _custom_dimensions[ "dimension2"] = clean_custom_dimension_value( _coordinator_model)
+
+    # Plugin Version
+    _plugin_version = self.pluginParameters.get("PluginVersion")
+    if _plugin_version:
+        _custom_dimensions[ "dimension3"] = clean_custom_dimension_value( _plugin_version)
+
+    # Coordinator Firmware Version
+    _coordinator_version = self.pluginParameters.get("DisplayFirmwareVersion")
+    if _coordinator_version:
+        _custom_dimensions[ "dimension4"] = clean_custom_dimension_value( _coordinator_version)
+
+    # Network Size
+    total, router, end_devices = get_network_size_items(self.pluginParameters.get("NetworkSize"))
+    if total:
+        _custom_dimensions[ "dimension5"] = clean_custom_dimension_value(total)
+
+    # Certified Db Version
+    certified_db_version = self.pluginParameters.get("CertifiedDbVersion")
+    if certified_db_version:
+        _custom_dimensions[ "dimension6"] = clean_custom_dimension_value( certified_db_version)
+
+    # OS Distribution
+    _distribution = get_distribution()
+    if _distribution:
+        _custom_dimensions[ "dimension7"] = clean_custom_dimension_value( _distribution)
+
+    # Platform Architecture
+    _archi = get_architecture_model(self)
+    if _archi:
+        _custom_dimensions[ "dimension8"] = clean_custom_dimension_value( _archi)
+
+    # Uptime
+    _uptime = get_uptime_category(self.statistics._start)
+    if _uptime:
+        _custom_dimensions[ "dimension9"] = clean_custom_dimension_value( _uptime)
+
+    # Ronelab model
+    ronelab_model = get_ronelabs_model_custom_definition()
+    if ronelab_model:
+        _custom_dimensions[ "dimension10"] = clean_custom_dimension_value(ronelab_model)
+
+    # Platform Id ( Pi Model )
+    pi_model = get_raspberry_pi_model()
+    if pi_model:
+        _custom_dimensions[ "dimension11"] = clean_custom_dimension_value(pi_model)
+
+    return _custom_dimensions
 
 
 def sending_plugin_analytics_infos(self):
-    
-    send_matomo_request(self, (ACTION_NAME + "_1"), None, populate_custom_dimmensions(self))
-    send_matomo_request(self, (ACTION_NAME + "_2"), populate_custom_variables_pack1(self), None)
-    send_matomo_request(self, (ACTION_NAME + "_3"), populate_custom_variables_pack2(self), None)
+    send_matomo_request(self, "Z4DPluginInfos", None, populate_custom_dimmensions(self))
 
 
 def send_matomo_request(self, action_name, custom_variable, custom_dimension):
-    
+
     client_id = get_clientid(self)
-    self.log.logging( "Matomo", "Log", f"send_matomo_request - Clien_id {client_id}")
+    self.log.logging( "Matomo", "Debug", f"send_matomo_request - Clien_id {client_id}")
     if client_id is None:
         self.log.logging( "Matomo", "Error", "Noting reported as MacAddress is None!")
         return
@@ -84,14 +115,15 @@ def send_matomo_request(self, action_name, custom_variable, custom_dimension):
         "action_name": action_name,
         "uid": client_id,
     }
-    
+
     if custom_variable:
         payload[ "cvar"] = json.dumps(custom_variable)
-        
+
     # Add Custom Dimensions to the request
     if custom_dimension:
         payload.update(custom_dimension)
 
+    self.log.logging( "Matomo", "Debug", f"send_matomo_request - payload {payload}")
     fetch_data_with_timeout(self, MATOMO_URL, payload)
 
 
@@ -101,7 +133,7 @@ def fetch_data_with_timeout(self, url, params, connect_timeout=3, read_timeout=5
         response.raise_for_status()  # Raise HTTPError for bad responses (4xx and 5xx)
 
         if response.status_code == 200:
-            self.log.logging( "Matomo", "Log", f"send_matomo_request - Request sent successfully! {response}")
+            self.log.logging( "Matomo", "Debug", f"send_matomo_request - Request sent successfully! {response}")
 
         else:
             self.log.logging( "Matomo", "Error", f"send_matomo_request - Failed to send request. Status code: {response.status_code}")
@@ -114,56 +146,121 @@ def fetch_data_with_timeout(self, url, params, connect_timeout=3, read_timeout=5
         self.log.logging( "Matomo", "Error",f"Request failed: {e}")
 
 
-
-def get_os_info():
-    os_name = platform.system()
-    if os_name == "Linux":
-        try:
-            with open("/etc/os-release") as f:
-                lines = f.readlines()
-                os_info = {line.split('=')[0]: line.split('=')[1].strip().strip('"') for line in lines if '=' in line}
-            return os_info.get("NAME", "Unknown"), os_info.get("VERSION", "Unknown")
-
-        except Exception as e:
-            return "Linux", "Unknown"
-
-    elif os_name == "Windows":
-        return "Windows", platform.version()
-
-    elif os_name == "Darwin":
-        return "macOS", platform.mac_ver()[0]
-
-    else:
-        return os_name, "Unknown"
-
-
-def get_architecture_model():
+def get_architecture_model(self):
     """
     Retrieve the architecture model of the current Python runtime and system.
 
     Returns:
-        dict: A dictionary containing architecture information.
+        str: A string containing architecture information.
     """
-    return {
-        "python_version": platform.python_version(),
-        "architecture": platform.architecture()[0],
-        "platform": platform.system(),
-        "machine": platform.machine(),
-        "processor": platform.processor(),
-        "python_architecture": "64-bit" if sys.maxsize > 2**32 else "32-bit",
-    }
+    try:
+        return f"python: {platform.python_version()} arch: {platform.architecture()[0]} machine: {platform.machine()} processor:{platform.processor()}"
+
+    except Exception as e:
+        self.log.logging( "Matomo", "Error", f"error {e}")
+
+    return None
 
 
 def get_ronelabs_model_custom_definition():
-
-    RONELABS_DIMENSION = {
-        "Ronelabs-GW-G5Lite": "dimension11",
-        "Ronelabs-GW-G5Mini": "dimension12"
-    }
     with open(RONELABS_MODEL_INFO) as f:
-        ronelabs_model = f.readline().strip()
-    
-    if ronelabs_model in RONELABS_DIMENSION:
-        return RONELABS_DIMENSION[ ronelabs_model ], ronelabs_model
-    
-    return None, None
+        return f.readline().strip()
+
+    return None
+
+
+def classify_uptime(uptime_seconds):
+    # Define thresholds in seconds for each category
+    thresholds = [
+        (1 * 86400, "1 day"),
+        (2 * 86400, "2 days"),
+        (3 * 86400, "3 days"),
+        (4 * 86400, "4 days"),
+        (5 * 86400, "5 days"),
+        (7 * 86400, "1 week"),
+        (14 * 86400, "2 weeks"),
+        (21 * 86400, "3 weeks"),
+        (28 * 86400, "4 weeks"),
+        (30 * 86400, "1 month"),
+        (60 * 86400, "2 months"),
+        (90 * 86400, "3 months"),
+        (120 * 86400, "4 months"),
+        (150 * 86400, "5 months"),
+        (180 * 86400, "6 months"),
+    ]
+
+    return next(
+        (
+            label
+            for threshold, label in thresholds
+            if uptime_seconds <= threshold
+        ),
+        "Beyond 6 months",
+    )
+
+
+def get_uptime_category(start_time):
+    # Calculate uptime in seconds
+    uptime_seconds = time.time() - start_time
+    return classify_uptime(uptime_seconds)
+
+
+def get_network_size_items(networksize):
+
+    if not networksize:
+        return None, None, None
+
+    # Split the string by " | " to separate each category
+    parts = networksize.split(" | ")
+
+    # Extract individual values using string splitting
+    try:
+        networkTotalsize = int(parts[0].split(":")[1].strip())  # Extract the number after "Total: "
+        networkRoutersize = int(parts[1].split(":")[1].strip())  # Extract the number after "Routers: "
+        networkEndDevicesize = int(parts[2].split(":")[1].strip())  # Extract the number after "End Devices: "
+
+    except IndexError:
+        print("Error: The string format doesn't match the expected pattern.")
+        return None, None, None
+
+    return classify_nwk_size(networkTotalsize), classify_nwk_size(networkRoutersize), classify_nwk_size(networkEndDevicesize)
+
+
+def classify_nwk_size(value):
+    if value < 5:
+        return "Micro"
+    elif 5 <= value < 10:
+        return "Small"
+    elif 10 <= value < 25:
+        return "Medium"
+    elif 25 <= value < 50:
+        return "Large"
+    elif 50 <= value < 75:
+        return "Very Large"
+    else:
+        return "Xtra Large"
+
+
+def get_distribution():
+    return f"{distro.name()} {distro.version()}"
+
+
+def clean_custom_dimension_value(value: str) -> str:
+    # Define a regex pattern for allowed characters: alphanumeric, spaces, underscores, hyphens, and periods
+    allowed_pattern = re.compile(r'[^a-zA-Z0-9 _.-]')
+
+    # Replace disallowed characters with a space
+    cleaned_value = re.sub(allowed_pattern, ' ', value)
+
+    # Collapse multiple spaces into a single space
+    cleaned_value = re.sub(r'\s{2,}', ' ', cleaned_value)
+
+    # Optionally, strip leading and trailing spaces
+    return cleaned_value.strip()
+
+
+def get_raspberry_pi_model():
+    if os.path.exists( DEVICE_TREE_CONFIGURATION ):
+        with open( DEVICE_TREE_CONFIGURATION , 'r') as f:
+            return f.read().strip()
+    return None
