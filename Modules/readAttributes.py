41284771eced532dd5e48e97f3755ef61f0c8fb5
Patrick Pichon <patrick@pichon.me>
2025-01-16 09:54:41 +0100

    make Zlinky related readAttributes debug based on Zlinky or ReadAttributes

--- Modules/readAttributes.py
+++ Modules/readAttributes.py
@@ -1346,9 +1346,10 @@ def ReadAttributeReq_ZLinky(self, nwkid):
     self.log.logging("ReadAttributes", "Debug", "ReadAttributeReq_ZLinky: " + nwkid + " EPout = " + EPout, nwkid=nwkid)
 
     for cluster in ( "0702", "0b01", "0b04" ):
-        self.log.logging("ZLinky", "Debug", "ReadAttributeReq_ZLinky: " + nwkid + " EPout = " + EPout + " Cluster = " + cluster, nwkid=nwkid)
+        self.log.logging(["ReadAttributes", "ZLinky"], "Debug", "ReadAttributeReq_ZLinky: " + nwkid + " EPout = " + EPout + " Cluster = " + cluster, nwkid=nwkid)
         listAttributes = retreive_ListOfAttributesByCluster(self, nwkid, EPout, cluster)
         ReadAttributeReq(self, nwkid, ZIGATE_EP, EPout, cluster, listAttributes, ackIsDisabled=False)
+
 
 def ReadAttributeReq_Scheduled_ZLinky(self, nwkid):
     # - La couleur du jour est déterminée au fur et à mesure de l'année et est diffusée par Edf la veille vers 12h.
@@ -1370,7 +1371,7 @@ def ReadAttributeReq_Scheduled_ZLinky(self, nwkid):
 
     EPout = "01"
     for cluster in WORK_TO_BE_DONE:
-        self.log.logging("ZLinky", "Log", "ReadAttributeReq_Scheduled_ZLinky: %s cluster %s attribute: %s" %( 
+        self.log.logging(["ReadAttributes", "ZLinky"], "Log", "ReadAttributeReq_Scheduled_ZLinky: %s cluster %s attribute: %s" %( 
             nwkid, cluster, WORK_TO_BE_DONE[ cluster ]), nwkid=nwkid)
         ReadAttributeReq(self, nwkid, ZIGATE_EP, EPout, cluster, WORK_TO_BE_DONE[ cluster ], ackIsDisabled=False)
    
@@ -1394,8 +1395,9 @@ def ReadAttributeRequest_0702_ZLinky_TIC(self, key):
         else:
             listAttributes = [0x0020, 0x0100, 0x0102, 0x0104, 0x0106, 0x0108, 0x10A]
 
-    self.log.logging("ReadAttributes", "ZLinky", "Request ZLinky infos on 0x0702 cluster: " + key + " EPout = " + EPout, nwkid=key)
+    self.log.logging(["ReadAttributes", "ZLinky"], "ZLinky", "Request ZLinky infos on 0x0702 cluster: " + key + " EPout = " + EPout, nwkid=key)
     ReadAttributeReq(self, key, ZIGATE_EP, EPout, "0702", listAttributes, ackIsDisabled=False)
+
 
 def ReadAttribute_ZLinkyIndex( self, nwkid ):
     # This can be used as a backup if the reporting do not work
@@ -1414,8 +1416,8 @@ def ReadAttribute_ZLinkyIndex( self, nwkid ):
         nwkid, "0702", INDEX_ATTRIBUTES[ optarif ], optarif), nwkid=nwkid)
     if optarif in INDEX_ATTRIBUTES:
         ReadAttributeReq(self, nwkid, ZIGATE_EP, EPout, "0702", INDEX_ATTRIBUTES[ optarif ], ackIsDisabled=False)
-        
-    
+
+
 def ReadAttributeRequest_0702_PC321(self, key):
     
     # Cluster 0x0702 Metering / Specific 0x0000
@@ -1699,12 +1701,12 @@ def ReadAttributeRequest_ff66(self, key):
     # Cluster ZLinky
 
     self.log.logging("ReadAttributes", "Debug", "ReadAttributeRequest_ff66 - Key: %s " % key, nwkid=key)
-    EPout = "01"
-    listAttributes = retreive_ListOfAttributesByCluster(self, key, EPout, "ff66")
-    self.log.logging("ReadAttributes", "Debug", "ReadAttributeRequest_ff66 - Key: %s request %s" % (
-        key, listAttributes), nwkid=key)
+    ep_out = "01"
+    attribute_list = retreive_ListOfAttributesByCluster(self, key, ep_out, "ff66")
+    self.log.logging(["ReadAttributes", "ZLinky"], "Debug", "ReadAttributeRequest_ff66 - Key: %s request %s" % (
+        key, attribute_list), nwkid=key)
 
-    ReadAttributeReq(self, key, ZIGATE_EP, EPout, "ff66", listAttributes, ackIsDisabled=is_ack_tobe_disabled(self, key))
+    ReadAttributeReq(self, key, ZIGATE_EP, ep_out, "ff66", attribute_list, ackIsDisabled=is_ack_tobe_disabled(self, key))
 
 def ReadAttributeRequest_fc7d(self, key):
     # Cluster IKEA
