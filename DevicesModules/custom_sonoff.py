9c76be6a6fc4dcd21c7ed1fcadbba10f7996a384
Patrick Pichon <patrick@pichon.me>
2025-03-14 12:44:29 +0100

    adding more parameters for Sonoff SWV valve

--- DevicesModules/custom_sonoff.py
+++ DevicesModules/custom_sonoff.py
@@ -23,8 +23,15 @@ SONOFF_MIN_TEMP = "0004"
 SONOFF_MAX_HUMI = "0005"
 SONOFF_MIN_HUMI = "0006"
 
+# Sonoff Smart Valve - SWV model
+SONOFF_REALTIME_IRRIGATION_DURATION = "5006"
+SONOFF_REALTIME_IRRIGATION_VOLUME = "5007"
+SONOFF_VALVE_ABNORMAL_STATE = "500c"
+SONOFF_IRRIGATIM_START_TIME = "500d"
+SONOFF_IRRIGATION_END_TIME = "500e"
+SONOFF_DAILY_IRRIGATION_VOLUME = "500F"
+SONOFF_VALVE_WORK_STATE = "5010"
 SONOFF_WATER_CLOSE_VALVE_TIMEOUT_ATTRIBUTE = "5011"
-
 
 def is_sonoff_device(self, nwkid):
     return self.ListOfDevices[nwkid]["Manufacturer"] == SONOFF_MANUFACTURER_ID or self.ListOfDevices[nwkid]["Manufacturer Name"] == SONOFF_MAUFACTURER_NAME
@@ -53,12 +60,30 @@ def sonoff_temp_humi_ranges(self, nwkid, value):
     write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_MIN_HUMI, "21", "%04x" %humi_min, ackIsDisabled=False)
 
 
+def sonoff_realtime_irrigation_duration(self, nwkid, value):
+    """ Real-time Irrigation duration """
+    self.log.logging("Sonoff", "Debug", "sonoff_realtime_irrigation_duration - Nwkid: %s value: %s" % (nwkid, value))
+    write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_REALTIME_IRRIGATION_DURATION, "23", "%08x" %value, ackIsDisabled=False)
+
+
+def sonoff_realtime_irrigation_volume(self, nwkid, value):
+    """ Real-time Irrigation volume """
+    self.log.logging("Sonoff", "Debug", "sonoff_realtime_irrigation_duration - Nwkid: %s value: %s" % (nwkid, value))
+    write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_REALTIME_IRRIGATION_VOLUME, "23", "%08x" %value, ackIsDisabled=False)
+
+
+def sonoff_realtime_irrigation_daily_volume(self, nwkid, value):
+    """ Daily irigation volume """
+    self.log.logging("Sonoff", "Debug", "sonoff_realtime_irrigation_duration - Nwkid: %s value: %s" % (nwkid, value))
+    write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_DAILY_IRRIGATION_VOLUME, "23", "%08x" %value, ackIsDisabled=False)
+
+
 def auto_close_when_water_shortage(self, nwkid, value):
     """ Automatically shut down the water valve after the water shortage exceeds 30 minutes. """
 
     self.log.logging("Sonoff", "Debug", "auto_close_when_water_shortage - Nwkid: %s value: %s" % (nwkid, value))
     water_close_valve_timeout = "%04x" % value
-    write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_WATER_CLOSE_VALVE_TIMEOUT_ATTRIBUTE, "20", water_close_valve_timeout, ackIsDisabled=False)
+    write_attribute(self, nwkid, ZIGATE_EP, "01", SONOFF_CLUSTER_ID, SONOFF_MANUFACTURER_ID, "01", SONOFF_WATER_CLOSE_VALVE_TIMEOUT_ATTRIBUTE, "21", water_close_valve_timeout, ackIsDisabled=False)
 
 
 SONOFF_DEVICE_PARAMETERS = {
@@ -68,5 +93,8 @@ SONOFF_DEVICE_PARAMETERS = {
     "SONOFF_TEMP_MIN": sonoff_temp_humi_ranges,
     "SONOFF_HUMI_MAX": sonoff_temp_humi_ranges,
     "SONOFF_HUMI_MIN": sonoff_temp_humi_ranges,
+    "SONOFF_REALTIME_IRRIGATION_DURATION": sonoff_realtime_irrigation_duration,
+    "SONOFF_REALTIME_IRRIGATION_VOLUME": sonoff_realtime_irrigation_volume,
+    "SONOFF_DAILY_IRRIGATION_VOLUME": sonoff_realtime_irrigation_daily_volume,
     "SONOFF_SWV_WATER_CLOSE_VALVE_TIMEOUT": auto_close_when_water_shortage
 }
