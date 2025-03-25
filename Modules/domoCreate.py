58c827973c4ff71bf7557df54602c80f88c530f2
Patrick Pichon <patrick@pichon.me>
2025-03-25 21:33:17 +0100

    Create only 1 P1Meter HC/HP and then use fake ep to distribute the Usage to different if needed (as for Tempo color)

--- Modules/domoCreate.py
+++ Modules/domoCreate.py
@@ -810,9 +810,7 @@ SIMPLE_WIDGET = {
         "Subtype": 1,
         "Switchtype": 1
     },
-    "P1Meter_1": { "Type": 250, "Subtype": 1, "Switchtype": 1 },
-    "P1Meter_2": { "Type": 250, "Subtype": 1, "Switchtype": 1 },
-    "P1Meter_3": { "Type": 250, "Subtype": 1, "Switchtype": 1 },
+    "P1Meter_HPHC": { "Type": 250, "Subtype": 1, "Switchtype": 1 },
     "P1Meter_ZL": {
         "Type": 250,
         "Subtype": 1,
