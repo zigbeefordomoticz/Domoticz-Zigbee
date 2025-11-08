063e6faca5477af36f75daf585623b69843c903a
Patrick Pichon <patrick@pichon.me>
2025-11-08 10:12:23 +0100

    Update OP Tarifaire for Zlinky

--- Modules/zlinky.py
+++ Modules/zlinky.py
@@ -105,8 +105,10 @@ OP_TARIFAIRE_MAP = {
     "HC..": (1, "Off-peak Hours"),
     "HP..": (2, "Peak Hours"),
     "HEURES CREUSES": (1, "Off-peak Hours"),
+    "HEURE  CREUSE": (1, "Off-peak Hours"),
     "HEURE CREUSE": (1, "Off-peak Hours"),
     "HEURES PLEINES": (2, "Peak Hours"),
+    "HEURE  PLEINE": (2, "Peak Hours"),
     "HEURE PLEINE": (2, "Peak Hours"),
 
     "HN..": (1, "Normal Hours"),
