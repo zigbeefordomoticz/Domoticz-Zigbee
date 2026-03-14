da4f35397ec39969504b668ae78f210d7ed6e21b
Patrick Pichon <patrick@pichon.me>
2026-03-14 14:01:55 +0100

    leave default response to be handle by the tuya code

--- Zigbee/zclDecoders.py
+++ Zigbee/zclDecoders.py
@@ -80,6 +80,10 @@ def send_default_rsp( self, fcf, disable_default_response, src_nwk_id, src_endpo
         self.log.logging("zclDecoder", "Debug", f"must_send_default_response returned False for command {command}", src_nwk_id)
         return
 
+    if cluster_id == "ef00":
+        # handle under tuya.tuyaReadRawAPS
+        pass
+
     self.log.logging(
         "zclDecoder",
         "Debug",
