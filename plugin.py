27f7351b25453f8e830c9e9f510b6b6b33dc843a
Patrick Pichon <patrick@pichon.me>
2026-03-29 16:19:00 +0200

    disable the sharded mode, as Domoticz crashs with multi-instances ( #1928 )

--- plugin.py
+++ plugin.py
@@ -11,7 +11,7 @@
 # SPDX-License-Identifier:    GPL-3.0 license
 
 """
-<plugin key="Zigate" name="Zigbee for domoticz plugin (zigpy enabled)" author="pipiche38" version="8.1" shared="true">
+<plugin key="Zigate" name="Zigbee for domoticz plugin (zigpy enabled)" author="pipiche38" version="8.1" shared="false">
     <description>
         <h1> Plugin Zigbee for domoticz</h1><br/>
             <br/><h2> Informations</h2><br/>
