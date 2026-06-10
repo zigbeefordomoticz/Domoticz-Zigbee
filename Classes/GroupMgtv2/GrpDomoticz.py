b4d9c24d67fb31a7767361c0edcdf46bdaa062af
Patrick Pichon <patrick@pichon.me>
2026-06-10 09:43:21 +0200

    missing covering level% info in debug

--- Classes/GroupMgtv2/GrpDomoticz.py
+++ Classes/GroupMgtv2/GrpDomoticz.py
@@ -417,8 +417,8 @@ def update_domoticz_group_device(self, GroupId):
 
         _collect_device_state(self, NwkId, Ep, group_cluster, acc, switchType, GroupId)
 
-        self.logging("Debug", "update_domoticz_group_device - Processing: Group: %s with device: %s/%s On: %s, Off: %s Stop: %s, level: %s" % (
-            GroupId, NwkId, Ep, acc.count_on, acc.count_off, acc.count_stop, acc.level))
+        self.logging("Debug", "update_domoticz_group_device - Processing: Group: %s with device: %s/%s On: %s, Off: %s Stop: %s, level: %s covering: %s" % (
+            GroupId, NwkId, Ep, acc.count_on, acc.count_off, acc.count_stop, acc.level, acc.covering_count))
 
     current_nValue, current_sValue = domo_read_nValue_sValue(self, self.Devices, GroupId, unit)
 
